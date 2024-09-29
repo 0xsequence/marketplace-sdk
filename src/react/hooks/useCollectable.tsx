baa9167d5184721b21f3ef70fac37c70449a932f
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-09-29 10:58:36 +0200

    Use collectable

---
+++ src/react/hooks/useCollectable.tsx
@@ -0,0 +1,40 @@
+import { collectableKeys } from '@api/queryKeys';
+import { getMetadataClient } from '@api/services';
+import { queryOptions, useQuery } from '@tanstack/react-query';
+import type { SdkConfig } from '@types';
+import { useConfig } from './useConfig';
+
+export type UseCollectableArgs = {
+	chainId: string;
+	collectionAddress: string;
+	tokenId: string;
+};
+
+const fetchCollectable = async (
+	args: UseCollectableArgs,
+	config: SdkConfig,
+) => {
+	const metadataClient = getMetadataClient(config);
+	return metadataClient
+		.getTokenMetadata({
+			chainID: args.chainId,
+			contractAddress: args.collectionAddress,
+			tokenIDs: [args.tokenId],
+		})
+		.then((resp) => resp.tokenMetadata[0]!);
+};
+
+export const collectableOptions = (
+	args: UseCollectableArgs,
+	config: Config,
+) => {
+	return queryOptions({
+		queryKey: [collectableKeys.detail, args, config],
+		queryFn: () => fetchCollectable(args, config),
+	});
+};
+
+export const useCollectable = (args: UseCollectableArgs) => {
+	const config = useConfig();
+	return useQuery(collectableOptions(args, config));
+};
