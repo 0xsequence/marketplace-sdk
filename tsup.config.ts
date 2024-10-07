245a20f469cc2447d4f50f5af45379b5d1dd9f0c
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-10-07 11:53:45 +0200

    Seperate builds to add use client pragma

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-	entry: ['src/**/*@(ts|tsx)'],
-	dts: true,
-	sourcemap: true,
-	clean: true,
-	format: ['esm'],
-});
+export default defineConfig([
+	{
+		entry: ['src/**/index.ts', '!src/react/**'],
+		dts: true,
+		sourcemap: true,
+		clean: true,
+		format: ['esm'],
+	},
+	{
+		entry: ['src/**/index.ts', '!src/react/ssr/index.ts'],
+		dts: true,
+		sourcemap: true,
+		format: ['esm'],
+		esbuildOptions(options) {
+			options.banner = {
+				js: '"use client"',
+			};
+		},
+	},
+	{
+		entry: { 'react/ssr/index': 'src/react/ssr/index.ts' },
+		dts: true,
+		sourcemap: true,
+		format: ['esm'],
+	},
+]);
