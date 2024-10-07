92bfaa6149252218467390bf9ed76900568af938
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-10-07 13:14:43 +0200

    Check for ssr rendering

--- src/react/provider.tsx
+++ src/react/provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ThemeProvider } from '@0xsequence/design-system';
-import { createContext, useState } from 'react';
+import { createContext } from 'react';
 import '@0xsequence/design-system/styles.css';
 import { type KitConfig, KitProvider } from '@0xsequence/kit';
 import { getQueryClient } from '@internal';
@@ -63,18 +63,20 @@ const Providers = ({
 		},
 	} satisfies KitConfig;
 
-	const [wagmiConfig] = useState(createWagmiConfig(marketplaceConfig, config));
+	const wagmiConfig = createWagmiConfig(
+		marketplaceConfig,
+		config,
+		!!initialState,
+	);
 
 	return (
 		<ThemeProvider>
 			<WagmiProvider config={wagmiConfig} initialState={initialState?.wagmi}>
 				<QueryClientProvider client={queryClient}>
 					<KitProvider config={kitConfig}>
-						<QueryClientProvider client={queryClient}>
-							<MarketplaceSdkContext.Provider value={config}>
-								<div id={PROVIDER_ID}>{children}</div>
-							</MarketplaceSdkContext.Provider>
-						</QueryClientProvider>
+						<MarketplaceSdkContext.Provider value={config}>
+							<div id={PROVIDER_ID}>{children}</div>
+						</MarketplaceSdkContext.Provider>
 					</KitProvider>
 				</QueryClientProvider>
 			</WagmiProvider>
