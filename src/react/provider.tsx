94c09f5903f4356a76bae1b56d2cbe515c2839a1
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-10-15 18:51:11 +0200

    Remove providers from sdk

--- src/react/provider.tsx
+++ src/react/provider.tsx
@@ -1,85 +1,24 @@
 'use client';
 
-import { ThemeProvider } from '@0xsequence/design-system';
 import { createContext } from 'react';
 import '@0xsequence/design-system/styles.css';
-import { type KitConfig, KitProvider } from '@0xsequence/kit';
-import { getQueryClient } from '@internal';
-import { marketplaceConfigOptions } from '@react-hooks/useMarketplaceConfig';
-import { QueryClientProvider, useQuery } from '@tanstack/react-query';
-import type { MarketplaceConfig, SdkConfig } from '@types';
-import { type State, WagmiProvider } from 'wagmi';
+import type { SdkConfig } from '@types';
 import { PROVIDER_ID } from './_internal/get-provider';
-import { createWagmiConfig } from './_internal/wagmi/create-config';
 
 export const MarketplaceSdkContext = createContext({} as SdkConfig);
 
 export type MarketplaceSdkProviderProps = {
 	config: SdkConfig;
 	children: React.ReactNode;
-	initialState?: { wagmi?: State };
 };
 
-const queryClient = getQueryClient();
-
-export function MarketplaceSdkProvider({
+export function MarketplaceProvider({
 	config,
 	children,
-	initialState,
 }: MarketplaceSdkProviderProps) {
-	const { data: marketplaceConfig } = useQuery(
-		marketplaceConfigOptions(config),
-		queryClient,
-	);
-
-	return marketplaceConfig ? (
-		<Providers
-			config={config}
-			marketplaceConfig={marketplaceConfig}
-			initialState={initialState}
-		>
-			{children}
-		</Providers>
-	) : (
-		<></>
+	return (
+		<MarketplaceSdkContext.Provider value={config}>
+			<div id={PROVIDER_ID}>{children}</div>
+		</MarketplaceSdkContext.Provider>
 	);
 }
-
-const Providers = ({
-	config,
-	marketplaceConfig,
-	children,
-	initialState,
-}: {
-	config: SdkConfig;
-	marketplaceConfig: MarketplaceConfig;
-	children: React.ReactNode;
-	initialState?: { wagmi?: State };
-}) => {
-	const kitConfig = {
-		projectAccessKey: config.projectAccessKey,
-		signIn: {
-			projectName: marketplaceConfig.title,
-		},
-	} satisfies KitConfig;
-
-	const wagmiConfig = createWagmiConfig(
-		marketplaceConfig,
-		config,
-		!!initialState,
-	);
-
-	return (
-		<ThemeProvider>
-			<WagmiProvider config={wagmiConfig} initialState={initialState?.wagmi}>
-				<QueryClientProvider client={queryClient}>
-					<KitProvider config={kitConfig}>
-						<MarketplaceSdkContext.Provider value={config}>
-							<div id={PROVIDER_ID}>{children}</div>
-						</MarketplaceSdkContext.Provider>
-					</KitProvider>
-				</QueryClientProvider>
-			</WagmiProvider>
-		</ThemeProvider>
-	);
-};
