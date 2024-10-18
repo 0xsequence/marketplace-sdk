3d130ea014571398e0618330c1297b7b0ea6ffb5
Mithat Akbulut <marmarisco860@gmail.com>
2024-10-18 16:04:19 +0300

    Export modal hooks for unified access
    - Added exports for modal hooks from respective modules.
    - Includes `useCreateListingModal`, `useMakeOfferModal`, `useSuccessfulPurchaseModal`, and `useTransferModal`.

---
+++ src/react/ui/index.ts
@@ -0,0 +1,5 @@
+export * from "./modals/modal-provider";
+export { useCreateListingModal } from "./modals/CreateListingModal";
+export { useMakeOfferModal } from "./modals/MakeOfferModal";
+export { useSuccessfulPurchaseModal } from "./modals/SuccessfulPurchaseModal";
+export { useTransferModal } from "./modals/TransferModal";
