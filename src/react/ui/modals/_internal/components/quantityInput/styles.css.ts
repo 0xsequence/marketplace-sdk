5bf033cc5a197e8205f83e11cb20f491a0aec572
Mithat Akbulut <marmarisco860@gmail.com>
2024-10-18 15:52:43 +0300

    Implement QuantityInput component with increment and decrement functionality

---
+++ src/react/ui/modals/_internal/components/quantityInput/styles.css.ts
@@ -0,0 +1,31 @@
+import { globalStyle, style } from '@vanilla-extract/css';
+
+export const quantityInputWrapper = style({
+	display: 'flex',
+	flexDirection: 'column',
+	width: '100%',
+	margin: '16px 0 2px 0',
+});
+
+globalStyle(`${quantityInputWrapper} > label`, {
+	gap: 2,
+});
+
+globalStyle(`${quantityInputWrapper} > label > div > div`, {
+	height: 36,
+	fontSize: 12,
+	borderRadius: 4,
+	paddingLeft: 12,
+	paddingRight: 0,
+});
+
+globalStyle(
+	`${quantityInputWrapper} > label > div > div:has(:disabled) , ${quantityInputWrapper} > label > div > div:has(:disabled):hover`,
+	{
+		opacity: 1,
+	},
+);
+
+globalStyle(`${quantityInputWrapper} > label > div > div > input`, {
+	fontSize: 12,
+});
