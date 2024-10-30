a956316791a537132c63dc26a6d1c89d171ef449
Mithat Akbulut <marmarisco860@gmail.com>
2024-10-30 11:58:48 +0300

    control showing modals in provider level

--- src/react/ui/modals/modal-provider.tsx
+++ src/react/ui/modals/modal-provider.tsx
@@ -1,3 +1,4 @@
+import { observer } from '@legendapp/state/react';
 import SwitchNetworkModal from './_internal/components/switchNetworkModal';
 import TransactionStatusModal from './_internal/components/transactionStatusModal';
 import { AccountModal } from './Account';
@@ -6,20 +7,26 @@ import { MakeOfferModal } from './MakeOfferModal';
 import { ReceivedOfferModal } from './ReceivedOfferModal';
 import SuccessfulPurchaseModal from './SuccessfulPurchaseModal';
 import { TransferModal } from './TransferModal';
+import { _accountModalOpen$ } from './_internal/stores/accountModal';
+import { createListingModal$ } from './CreateListingModal/_store';
+import { makeOfferModal$ } from './MakeOfferModal/_store';
+import { transferModal$ } from './TransferModal/_store';
+import { receivedOfferModal$ } from './ReceivedOfferModal/_store';
+import { successfulPurchaseModal$ } from './SuccessfulPurchaseModal/_store';
 
-export const ModalProvider = () => {
+export const ModalProvider = observer(() => {
 	return (
 		<>
-			<AccountModal />
-			<CreateListingModal />
-			<MakeOfferModal />
-			<TransferModal />
-			<ReceivedOfferModal />
-			<SuccessfulPurchaseModal />
+			{_accountModalOpen$.get() && <AccountModal />}
+			{createListingModal$.isOpen.get() && <CreateListingModal />}
+			{makeOfferModal$.isOpen.get() && <MakeOfferModal />}
+			{transferModal$.isOpen.get() && <TransferModal />}
+			{receivedOfferModal$.isOpen.get() && <ReceivedOfferModal />}
+			{successfulPurchaseModal$.isOpen.get() && <SuccessfulPurchaseModal />}
 
 			{/* Helper Modals */}
 			<SwitchNetworkModal />
 			<TransactionStatusModal />
 		</>
 	);
-};
+});
