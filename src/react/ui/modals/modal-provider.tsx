4bfe5f820661cef201b7e89d555db1ff3e274acc
Mithat Akbulut <marmarisco860@gmail.com>
2024-11-01 15:18:16 +0300

    change ModalProvider back since we not have control in modals to show them

--- src/react/ui/modals/modal-provider.tsx
+++ src/react/ui/modals/modal-provider.tsx
@@ -8,36 +8,20 @@ import { SellModal } from './SellModal';
 import SuccessfulPurchaseModal from './SuccessfulPurchaseModal';
 import { TransferModal } from './TransferModal';
 import { _accountModalOpen$ } from './_internal/stores/accountModal';
-import { createListingModal$ } from './CreateListingModal/_store';
-import { makeOfferModal$ } from './MakeOfferModal/_store';
-import { transferModal$ } from './TransferModal/_store';
-import { sellModal$ } from './SellModal/_store';
-import { successfulPurchaseModal$ } from './SuccessfulPurchaseModal/_store';
-const MODAL_CONFIG = [
-	{ condition: _accountModalOpen$, Component: AccountModal },
-	{ condition: createListingModal$.isOpen, Component: CreateListingModal },
-	{ condition: makeOfferModal$.isOpen, Component: MakeOfferModal },
-	{ condition: transferModal$.isOpen, Component: TransferModal },
-	{ condition: sellModal$.isOpen, Component: SellModal },
-	{
-		condition: successfulPurchaseModal$.isOpen,
-		Component: SuccessfulPurchaseModal,
-	},
-];
-
-const HELPER_MODALS = [SwitchChainModal, TransactionStatusModal];
 
 export const ModalProvider = observer(() => {
 	return (
 		<>
-			{MODAL_CONFIG.map(
-				({ condition, Component }, index) =>
-					condition.get() && <Component key={index} />,
-			)}
+			<AccountModal />
+			<CreateListingModal />
+			<MakeOfferModal />
+			<TransferModal />
+			<SellModal />
+			<SuccessfulPurchaseModal />
 
-			{HELPER_MODALS.map((HelperModal, index) => (
-				<HelperModal key={index} />
-			))}
+			{/* Helper modals */}
+			<SwitchChainModal />
+			<TransactionStatusModal />
 		</>
 	);
 });
