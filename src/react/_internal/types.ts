9f6ec4e33b03b9d5f7d1107d5c8f1bfe31401142
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-08 10:38:34 +0100

    Add zod types

--- src/react/_internal/types.ts
+++ src/react/_internal/types.ts
@@ -1,13 +1,25 @@
-import type { ChainId as NetworkChainId } from '@0xsequence/network';
+import { ChainId as NetworkChainId } from '@0xsequence/network';
 import type { ContractType } from '@types';
+import { z } from 'zod';
 
-export type QueryArg = {
-	query?: {
-		enabled?: boolean;
-		//TODO: Add more fields
-	};
-};
+export const QueryArgSchema = z
+	.object({
+		query: z
+			.object({
+				enabled: z.boolean().optional(),
+			})
+			.optional(),
+	})
+	.optional();
 
-export type ChainId = string | number | NetworkChainId;
+export type QueryArg = z.infer<typeof QueryArgSchema>;
+
+export const ChainIdSchema = z.union([
+	z.string(),
+	z.number(),
+	z.nativeEnum(NetworkChainId),
+]);
+
+export type ChainId = z.infer<typeof ChainIdSchema>;
 
 export type CollectionType = ContractType.ERC1155 | ContractType.ERC721;
