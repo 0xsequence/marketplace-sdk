d570b89c47e4bb7e3967a7b9467cf879ad8c8fbb
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-09 14:33:54 +0100

    Format

--- src/react/hooks/useGenerateListingTransaction.tsx
+++ src/react/hooks/useGenerateListingTransaction.tsx
@@ -1,63 +1,63 @@
 import {
-  type ChainId,
-  type GenerateListingTransactionArgs,
-  getMarketplaceClient,
-} from "@internal";
-import { useMutation } from "@tanstack/react-query";
-import type { SdkConfig, Step } from "@types";
-import { useConfig } from "./useConfig";
+	type ChainId,
+	type GenerateListingTransactionArgs,
+	getMarketplaceClient,
+} from '@internal';
+import { useMutation } from '@tanstack/react-query';
+import type { SdkConfig, Step } from '@types';
+import { useConfig } from './useConfig';
 
 export type UseGenerateListingTransactionArgs = {
-  chainId: ChainId;
-  onSuccess?: (data?: Step[]) => void;
+	chainId: ChainId;
+	onSuccess?: (data?: Step[]) => void;
 };
 
-import type { CreateReq } from "@types";
+import type { CreateReq } from '@types';
 
-export type CreateReqWithDateExpiry = Omit<CreateReq, "expiry"> & {
-  expiry: Date;
+export type CreateReqWithDateExpiry = Omit<CreateReq, 'expiry'> & {
+	expiry: Date;
 };
 
 export type GenerateListingTransactionProps = Omit<
-  GenerateListingTransactionArgs,
-  "listing"
+	GenerateListingTransactionArgs,
+	'listing'
 > & {
-  listing: CreateReqWithDateExpiry;
+	listing: CreateReqWithDateExpiry;
 };
 
 const dateToUnixTime = (date: Date) =>
-  Math.floor(date.getTime() / 1000).toString();
+	Math.floor(date.getTime() / 1000).toString();
 
 export const generateListingTransaction = async (
-  params: GenerateListingTransactionProps,
-  config: SdkConfig,
-  chainId: ChainId
+	params: GenerateListingTransactionProps,
+	config: SdkConfig,
+	chainId: ChainId,
 ) => {
-  const args = {
-    ...params,
-    listing: {
-      ...params.listing,
-      expiry: dateToUnixTime(params.listing.expiry),
-    },
-  } satisfies GenerateListingTransactionArgs;
-  const marketplaceClient = getMarketplaceClient(chainId, config);
-  return (await marketplaceClient.generateListingTransaction(args)).steps;
+	const args = {
+		...params,
+		listing: {
+			...params.listing,
+			expiry: dateToUnixTime(params.listing.expiry),
+		},
+	} satisfies GenerateListingTransactionArgs;
+	const marketplaceClient = getMarketplaceClient(chainId, config);
+	return (await marketplaceClient.generateListingTransaction(args)).steps;
 };
 
 export const useGenerateListingTransaction = (
-  params: UseGenerateListingTransactionArgs
+	params: UseGenerateListingTransactionArgs,
 ) => {
-  const config = useConfig();
+	const config = useConfig();
 
-  const { mutate, mutateAsync, ...result } = useMutation({
-    onSuccess: params.onSuccess,
-    mutationFn: (args: GenerateListingTransactionProps) =>
-      generateListingTransaction(args, config, params.chainId),
-  });
+	const { mutate, mutateAsync, ...result } = useMutation({
+		onSuccess: params.onSuccess,
+		mutationFn: (args: GenerateListingTransactionProps) =>
+			generateListingTransaction(args, config, params.chainId),
+	});
 
-  return {
-    ...result,
-    generateListingTransaction: mutate,
-    generateListingTransactionAsync: mutateAsync,
-  };
+	return {
+		...result,
+		generateListingTransaction: mutate,
+		generateListingTransactionAsync: mutateAsync,
+	};
 };
