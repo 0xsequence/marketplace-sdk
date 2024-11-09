7f93b350bba8ba15be4ba4fdc0b2c2cc231ff75f
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-09 12:23:25 +0100

    biome

--- src/types/marketplace-config.ts
+++ src/types/marketplace-config.ts
@@ -1,63 +1,64 @@
-import type { ChainId } from "@0xsequence/network";
+import type { ChainId } from '@0xsequence/network';
+import type { Hex } from 'viem';
 
 interface Socials {
-  twitter?: string;
-  website?: string;
-  discord?: string;
-  instagram?: string;
-  tiktok?: string;
-  youtube?: string;
+	twitter?: string;
+	website?: string;
+	discord?: string;
+	instagram?: string;
+	tiktok?: string;
+	youtube?: string;
 }
 
 enum WalletOptions {
-  Sequence = "sequence",
-  Metamask = "metamask",
-  WalletConnect = "walletconnect",
-  Coinbase = "coinbase",
-  Injected = "injected",
-  Ledger = "ledger",
-  Rainbow = "rainbow",
+	Sequence = 'sequence',
+	Metamask = 'metamask',
+	WalletConnect = 'walletconnect',
+	Coinbase = 'coinbase',
+	Injected = 'injected',
+	Ledger = 'ledger',
+	Rainbow = 'rainbow',
 }
 
-type NewWalletConnectors = "walletconnect" | "coinbase";
+type NewWalletConnectors = 'walletconnect' | 'coinbase';
 interface WalletOptionsNew {
-  connectors: NewWalletConnectors[];
-  includeEIP6963Wallets: boolean;
-  walletType: "embedded" | "universal";
+	connectors: NewWalletConnectors[];
+	includeEIP6963Wallets: boolean;
+	walletType: 'embedded' | 'universal';
 }
 
 interface Collection {
-  collectionAddress: Hex;
-  chainId: ChainId;
-  exchanges?: string[];
-  marketplaceFeePercentage: number;
-  bannerUrl?: string;
-  marketplaceType: "p2p" | "orderbook" | "amm";
-  currencyOptions?: string[];
+	collectionAddress: Hex;
+	chainId: ChainId;
+	exchanges?: string[];
+	marketplaceFeePercentage: number;
+	bannerUrl?: string;
+	marketplaceType: 'p2p' | 'orderbook' | 'amm';
+	currencyOptions?: string[];
 }
 
-type LandingPageLayout = "default" | "big_left_banner" | "floating_header";
+type LandingPageLayout = 'default' | 'big_left_banner' | 'floating_header';
 
 export interface MarketplaceConfig {
-  projectId: number; // builder project Id
-  publisherId: string;
-  title: string;
-  shortDescription: string;
-  socials?: Socials;
-  faviconUrl: string;
-  landingBannerUrl: string;
-  logoUrl?: string;
-  fontUrl?: string;
-  ogImage?: string;
-  titleTemplate: string;
-  disableLiquidityProviderTools?: boolean;
-  walletOptions: WalletOptions[];
-  walletOptionsNew?: WalletOptionsNew;
-  collections: Collection[];
-  landingPageLayout: LandingPageLayout;
-  // Appended in the sdk
-  cssString: string;
-  // Appended in the sdk
+	projectId: number; // builder project Id
+	publisherId: string;
+	title: string;
+	shortDescription: string;
+	socials?: Socials;
+	faviconUrl: string;
+	landingBannerUrl: string;
+	logoUrl?: string;
+	fontUrl?: string;
+	ogImage?: string;
+	titleTemplate: string;
+	disableLiquidityProviderTools?: boolean;
+	walletOptions: WalletOptions[];
+	walletOptionsNew?: WalletOptionsNew;
+	collections: Collection[];
+	landingPageLayout: LandingPageLayout;
+	// Appended in the sdk
+	cssString: string;
+	// Appended in the sdk
 
-  manifestUrl: string;
+	manifestUrl: string;
 }
