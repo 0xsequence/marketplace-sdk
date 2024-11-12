12ccbad19603b3e09b61f5acb227e0bd12f79b5b
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-12 10:37:14 +0100

    Fix access key

--- playgrounds/react-vite/src/config.ts
+++ playgrounds/react-vite/src/config.ts
@@ -2,5 +2,5 @@ import type { SdkConfig } from '@0xsequence/marketplace-sdk';
 
 export const sdkConfig = {
 	projectId: '34598',
-	pojectAccessKey: 'AQAAAAAAADVH8R2AGuQhwQ1y8NaEf1T7PJM',
+	projectAccessKey: 'AQAAAAAAADVH8R2AGuQhwQ1y8NaEf1T7PJM',
 } satisfies SdkConfig;
