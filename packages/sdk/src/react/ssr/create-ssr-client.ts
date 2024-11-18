91e85cdb61916d4ea02aae556e8bef35be2675d9
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-18 13:29:02 +0100

    do not use ts imports

--- packages/sdk/src/react/ssr/create-ssr-client.ts
+++ packages/sdk/src/react/ssr/create-ssr-client.ts
@@ -2,7 +2,7 @@ import { type State, cookieToInitialState } from 'wagmi';
 import type { SdkConfig } from '../../types/sdk-config';
 import { getQueryClient } from '../_internal/api/get-query-client';
 import { createWagmiConfig } from '../_internal/wagmi/create-config';
-import { marketplaceConfigOptions } from '../hooks/options/marketplaceConfigOptions.ts';
+import { marketplaceConfigOptions } from '../hooks/options/marketplaceConfigOptions';
 
 type InitSSRClientArgs = {
 	cookie: string;
