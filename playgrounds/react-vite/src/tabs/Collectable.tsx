ddbf15bda08e39af1256d3a7a52df26ea68fe104
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-24 12:41:00 +0100

    COnditionally render actions if wallet is connected

--- playgrounds/react-vite/src/tabs/Collectable.tsx
+++ playgrounds/react-vite/src/tabs/Collectable.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Button } from "@0xsequence/design-system";
+import { Box, Card, Button, Text } from "@0xsequence/design-system";
 import {
   useMakeOfferModal,
   useCreateListingModal,
@@ -7,21 +7,53 @@ import {
   CollectibleCard,
 } from "@0xsequence/marketplace-sdk/react";
 import { useMarketplace } from "../lib/MarketplaceContext";
+import { useAccount } from "wagmi";
 
 export function Collectible() {
+  const context = useMarketplace();
+
+  const { address } = useAccount();
+  return (
+    <Box paddingTop="3" gap="3">
+      <Card>
+        {address ? (
+          <Actions />
+        ) : (
+          <Text variant="large">Connect Wallet to see collectable actions</Text>
+        )}
+      </Card>
+      <CollectibleCard
+        chainId={Number(context.chainId)}
+        collectionAddress={context.collectionAddress}
+        tokenId={context.collectibleId}
+        onCollectibleClick={() => console.log("Collectible clicked")}
+        onOfferClick={() => console.log("Offer clicked")}
+      />
+    </Box>
+  );
+}
+
+function Actions() {
   const context = useMarketplace();
   const { show: openMakeOfferModal } = useMakeOfferModal();
   const { show: openCreateListingModal } = useCreateListingModal();
   const { show: openTransferModal } = useTransferModal();
   const { show: openSellModal } = useSellModal();
   return (
-    <Box paddingTop="3" gap="3">
-      <Card gap="3">
+    <Box gap="3">
+      <Box gap="3" flexDirection="column">
         <Button
           variant="primary"
           onClick={() => openMakeOfferModal(context)}
           label="Make Offer"
         />
+        <Button
+          variant="primary"
+          onClick={() => openCreateListingModal(context)}
+          label="Create Listing"
+        />
+      </Box>
+      <Box gap="3" flexDirection="column">
         <Button
           variant="primary"
           onClick={() => openCreateListingModal(context)}
@@ -38,14 +70,7 @@ export function Collectible() {
           }
           label="Transfer"
         />
-      </Card>
-      <CollectibleCard
-        chainId={Number(context.chainId)}
-        collectionAddress={context.collectionAddress}
-        tokenId={context.collectibleId}
-        onCollectibleClick={() => console.log("Collectible clicked")}
-        onOfferClick={() => console.log("Offer clicked")}
-      />
+      </Box>
     </Box>
   );
 }
