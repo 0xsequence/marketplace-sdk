5c29a11900ed410cbadb49a5dcd28bcb1a73a0ef
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-25 09:17:07 +0100

    Use generate buy transaction

--- packages/sdk/src/react/hooks/useGenerateBuyTransaction.tsx
+++ packages/sdk/src/react/hooks/useGenerateBuyTransaction.tsx
@@ -1,50 +1,50 @@
-import { useMutation } from "@tanstack/react-query";
-import { z } from "zod";
-import type { SdkConfig } from "../../types";
+import { useMutation } from '@tanstack/react-query';
+import { z } from 'zod';
+import type { SdkConfig } from '../../types';
 import {
-  type ChainId,
-  ChainIdSchema,
-  type GenerateBuyTransactionArgs,
-  getMarketplaceClient,
-} from "../_internal";
-import { stepSchema } from "../_internal/api/zod-schema";
-import { useConfig } from "./useConfig";
+	type ChainId,
+	ChainIdSchema,
+	type GenerateBuyTransactionArgs,
+	getMarketplaceClient,
+} from '../_internal';
+import { stepSchema } from '../_internal/api/zod-schema';
+import { useConfig } from './useConfig';
 
 const UserGeneratBuyTransactionArgsSchema = z.object({
-  chainId: ChainIdSchema.pipe(z.coerce.string()),
-  onSuccess: z.function().args(stepSchema.array().optional()).optional(),
+	chainId: ChainIdSchema.pipe(z.coerce.string()),
+	onSuccess: z.function().args(stepSchema.array().optional()).optional(),
 });
 
 type UseGenerateBuyTransactionArgs = z.infer<
-  typeof UserGeneratBuyTransactionArgsSchema
+	typeof UserGeneratBuyTransactionArgsSchema
 >;
 
 export const generateBuyTransaction = async (
-  args: GenerateBuyTransactionArgs,
-  config: SdkConfig,
-  chainId: ChainId
+	args: GenerateBuyTransactionArgs,
+	config: SdkConfig,
+	chainId: ChainId,
 ) => {
-  const parsedChainId = ChainIdSchema.pipe(z.coerce.string()).parse(chainId);
-  const marketplaceClient = getMarketplaceClient(parsedChainId, config);
-  return marketplaceClient
-    .generateBuyTransaction(args)
-    .then((data) => data.steps);
+	const parsedChainId = ChainIdSchema.pipe(z.coerce.string()).parse(chainId);
+	const marketplaceClient = getMarketplaceClient(parsedChainId, config);
+	return marketplaceClient
+		.generateBuyTransaction(args)
+		.then((data) => data.steps);
 };
 
 export const useGenerateBuyTransaction = (
-  params: UseGenerateBuyTransactionArgs
+	params: UseGenerateBuyTransactionArgs,
 ) => {
-  const config = useConfig();
+	const config = useConfig();
 
-  const { mutate, mutateAsync, ...result } = useMutation({
-    onSuccess: params.onSuccess,
-    mutationFn: (args: GenerateBuyTransactionArgs) =>
-      generateBuyTransaction(args, config, params.chainId),
-  });
+	const { mutate, mutateAsync, ...result } = useMutation({
+		onSuccess: params.onSuccess,
+		mutationFn: (args: GenerateBuyTransactionArgs) =>
+			generateBuyTransaction(args, config, params.chainId),
+	});
 
-  return {
-    ...result,
-    generateBuyTransaction: mutate,
-    generateBuyTransactionAsync: mutateAsync,
-  };
+	return {
+		...result,
+		generateBuyTransaction: mutate,
+		generateBuyTransactionAsync: mutateAsync,
+	};
 };
