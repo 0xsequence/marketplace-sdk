41dec31bf82a4daf53f58b8bd9b8aef7db0871ef
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-25 14:36:14 +0100

    Revert "TMP: remove buy modal"
    This reverts commit 9efdec1530c7a8aaaaa5290fbac140ded9fd9e32.

--- packages/sdk/src/react/ui/modals/modal-provider.tsx
+++ packages/sdk/src/react/ui/modals/modal-provider.tsx
@@ -1,13 +1,14 @@
 import { observer } from '@legendapp/state/react';
+import SwitchChainModal from './_internal/components/switchChainModal';
+import TransactionStatusModal from './_internal/components/transactionStatusModal';
 import { AccountModal } from './Account';
 import { CreateListingModal } from './CreateListingModal';
 import { MakeOfferModal } from './MakeOfferModal';
 import { SellModal } from './SellModal';
 import SuccessfulPurchaseModal from './SuccessfulPurchaseModal';
 import { TransferModal } from './TransferModal';
-import SwitchChainModal from './_internal/components/switchChainModal';
-import TransactionStatusModal from './_internal/components/transactionStatusModal';
 import { _accountModalOpen$ } from './_internal/stores/accountModal';
+import { BuyModal } from './BuyModal';
 
 export const ModalProvider = observer(() => {
 	return (
@@ -17,6 +18,7 @@ export const ModalProvider = observer(() => {
 			<MakeOfferModal />
 			<TransferModal />
 			<SellModal />
+			<BuyModal />
 			<SuccessfulPurchaseModal />
 			{/* Helper modals */}
 			<SwitchChainModal />
