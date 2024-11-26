6eae21873a831d3fc3f906980246fbb437d6847c
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-26 10:24:13 +0100

    Convert buy hook to query hook

--- packages/sdk/src/react/hooks/useGenerateBuyTransaction.tsx
+++ packages/sdk/src/react/hooks/useGenerateBuyTransaction.tsx
@@ -1,50 +1,71 @@
-import { useMutation } from '@tanstack/react-query';
-import { z } from 'zod';
-import type { SdkConfig } from '../../types';
+import { queryOptions, useQuery } from "@tanstack/react-query";
+import { z } from "zod";
+import type { SdkConfig } from "../../types";
 import {
-	type ChainId,
-	ChainIdSchema,
-	type GenerateBuyTransactionArgs,
-	getMarketplaceClient,
-} from '../_internal';
-import { stepSchema } from '../_internal/api/zod-schema';
-import { useConfig } from './useConfig';
+  AddressSchema,
+  ChainIdSchema,
+  getMarketplaceClient,
+  MarketplaceKind,
+  QueryArgSchema,
+  WalletKind,
+} from "../_internal";
+import { useConfig } from "./useConfig";
+import { useAccount } from "wagmi";
+import type { Hex } from "viem";
 
-const UserGeneratBuyTransactionArgsSchema = z.object({
-	chainId: ChainIdSchema.pipe(z.coerce.string()),
-	onSuccess: z.function().args(stepSchema.array().optional()).optional(),
+export const UseGenerateBuyTransactionArgsSchema = z.object({
+  chainId: ChainIdSchema.pipe(z.coerce.string()),
+  collectionAddress: AddressSchema,
+  marketplace: z.nativeEnum(MarketplaceKind),
+  ordersData: z.array(
+    z.object({
+      orderId: z.string(),
+      quantity: z.string(),
+      marketplace: z.string(),
+    })
+  ),
+  walletType: z.nativeEnum(WalletKind).optional(),
+  query: QueryArgSchema,
 });
 
 type UseGenerateBuyTransactionArgs = z.infer<
-	typeof UserGeneratBuyTransactionArgsSchema
+  typeof UseGenerateBuyTransactionArgsSchema
 >;
 
-export const generateBuyTransaction = async (
-	args: GenerateBuyTransactionArgs,
-	config: SdkConfig,
-	chainId: ChainId,
+export const fetchGenerateBuyTransaction = async (
+  args: UseGenerateBuyTransactionArgs & { buyer: Hex },
+  config: SdkConfig
 ) => {
-	const parsedChainId = ChainIdSchema.pipe(z.coerce.string()).parse(chainId);
-	const marketplaceClient = getMarketplaceClient(parsedChainId, config);
-	return marketplaceClient
-		.generateBuyTransaction(args)
-		.then((data) => data.steps);
+  const parsedArgs = UseGenerateBuyTransactionArgsSchema.parse(args);
+  const marketplaceClient = getMarketplaceClient(parsedArgs.chainId, config);
+
+  return marketplaceClient
+    .generateBuyTransaction({
+      ...parsedArgs,
+      buyer: args.buyer,
+      additionalFees: [], // TODO: Add additional fees
+    })
+    .then((data) => data.steps);
+};
+
+export const generateBuyTransactionOptions = (
+  args: UseGenerateBuyTransactionArgs & { buyer: Hex },
+  config: SdkConfig
+) => {
+  return queryOptions({
+    queryKey: ["generateBuyTransaction", args],
+    queryFn: () => fetchGenerateBuyTransaction(args, config),
+    ...args.query,
+  });
 };
 
 export const useGenerateBuyTransaction = (
-	params: UseGenerateBuyTransactionArgs,
+  args: UseGenerateBuyTransactionArgs
 ) => {
-	const config = useConfig();
-
-	const { mutate, mutateAsync, ...result } = useMutation({
-		onSuccess: params.onSuccess,
-		mutationFn: (args: GenerateBuyTransactionArgs) =>
-			generateBuyTransaction(args, config, params.chainId),
-	});
-
-	return {
-		...result,
-		generateBuyTransaction: mutate,
-		generateBuyTransactionAsync: mutateAsync,
-	};
+  const { address } = useAccount();
+  const config = useConfig();
+  return useQuery(
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    generateBuyTransactionOptions({ buyer: address!, ...args }, config)
+  );
 };
