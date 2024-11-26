bc257f71182398e5e4b6a9fc831664469d0e2027
Mithat Akbulut <marmarisco860@gmail.com>
2024-11-26 16:02:56 +0300

    refactor Footer component to not use hooks, instead get data with props

--- packages/sdk/src/react/ui/components/collectible-card/Footer.tsx
+++ packages/sdk/src/react/ui/components/collectible-card/Footer.tsx
@@ -1,121 +1,98 @@
-import { Box, IconButton, Image, Text } from '@0xsequence/design-system';
-import {
-	useBalanceOfCollectible,
-	useCurrencies,
-	useHighestOffer,
-	useLowestListing,
-} from '../../../hooks';
-import { useAccount } from 'wagmi';
-import { formatUnits, type Hex } from 'viem';
-import { ContractType } from '../../../_internal';
-import Pill from '../_internals/pill/Pill';
-import SvgBellIcon from '../../icons/Bell';
-import { footer, offerBellButton } from './styles.css';
+import { Box, IconButton, Image, Text } from "@0xsequence/design-system";
+import { formatUnits } from "viem";
+import { ContractType, Currency, Order } from "../../../_internal";
+import Pill from "../_internals/pill/Pill";
+import SvgBellIcon from "../../icons/Bell";
+import { footer, offerBellButton } from "./styles.css";
+import { TokenBalance } from "@0xsequence/indexer";
+import { useAccount } from "wagmi";
 
 type FooterProps = {
-	name: string;
-	type: ContractType;
-	chainId: string;
-	tokenId: string;
-	collectionAddress: Hex;
-	onOfferClick?: () => void;
+  name: string;
+  type?: ContractType;
+  onOfferClick?: () => void;
+  highestOffer?: Order;
+  lowestListing?: Order;
+  currency?: Currency;
+  balanceOfCollectible?: TokenBalance;
 };
 
 export const Footer = ({
-	name,
-	type,
-	chainId,
-	tokenId,
-	collectionAddress,
-	onOfferClick,
+  name,
+  type,
+  onOfferClick,
+  highestOffer,
+  lowestListing,
+  currency,
+  balanceOfCollectible,
 }: FooterProps) => {
-	const { address: accountAddress } = useAccount();
-	const { data: balance } = useBalanceOfCollectible({
-		chainId: String(chainId),
-		collectionAddress: collectionAddress,
-		collectableId: tokenId,
-		userAddress: accountAddress!,
-	});
-	const { data: lowestListing } = useLowestListing({
-		chainId: String(chainId),
-		collectionAddress: collectionAddress,
-		tokenId: tokenId,
-	});
-	const { data: highestOffer } = useHighestOffer({
-		chainId: String(chainId),
-		collectionAddress: collectionAddress,
-		tokenId: tokenId,
-	});
-	const { data: currencies } = useCurrencies({
-		chainId: String(chainId),
-		collectionAddress,
-		query: {
-			enabled: !!lowestListing?.order,
-		},
-	});
-	const currency = currencies?.find(
-		(currency) =>
-			currency.contractAddress === lowestListing?.order?.priceCurrencyAddress,
-	);
+  const { address } = useAccount();
 
-	if (name.length > 15 && highestOffer?.order) {
-		name = name.substring(0, 13) + '...';
-	}
-	if (name.length > 17 && !highestOffer?.order) {
-		name = name.substring(0, 17) + '...';
-	}
+  if (name.length > 15 && highestOffer) {
+    name = name.substring(0, 13) + "...";
+  }
+  if (name.length > 17 && !highestOffer) {
+    name = name.substring(0, 17) + "...";
+  }
 
-	return (
-		<Box
-			display="flex"
-			flexDirection="column"
-			gap="2"
-			padding="4"
-			whiteSpace="nowrap"
-			position="relative"
-			className={accountAddress ? footer.animated : footer.static}
-		>
-			<Box
-				display="flex"
-				alignItems="center"
-				justifyContent="space-between"
-				position="relative"
-			>
-				<Text color='text100' fontSize="normal" fontWeight="bold" textAlign="left">
-					{name}
-				</Text>
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap="2"
+      padding="4"
+      whiteSpace="nowrap"
+      position="relative"
+      className={!!address ? footer.animated : footer.static}
+    >
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        position="relative"
+      >
+        <Text
+          color="text100"
+          fontSize="normal"
+          fontWeight="bold"
+          textAlign="left"
+        >
+          {name}
+        </Text>
 
-				{highestOffer?.order && onOfferClick && (
-					<IconButton
-						variant="primary"
-						className={offerBellButton}
-						onClick={(e) => {
-							e.stopPropagation();
-							onOfferClick?.();
-						}}
-						icon={SvgBellIcon}
-					/>
-				)}
-			</Box>
+        {highestOffer && onOfferClick && (
+          <IconButton
+            variant="primary"
+            className={offerBellButton}
+            onClick={(e) => {
+              e.stopPropagation();
+              onOfferClick?.();
+            }}
+            icon={(props) => <SvgBellIcon {...props} size={"xs"} />}
+          />
+        )}
+      </Box>
 
-			{lowestListing?.order && currency && (
-				<Box display="flex" alignItems="center" gap="1">
-					<Image src={currency?.imageUrl} width="3" height="3" />
+      {lowestListing && currency && (
+        <Box display="flex" alignItems="center" gap="1">
+          <Image src={currency?.imageUrl} width="3" height="3" />
 
-					<Text color='text100' fontSize="small" fontWeight="bold" textAlign="left">
-						{formatUnits(
-							BigInt(lowestListing.order.priceAmount),
-							currency.decimals,
-						)}{' '}
-					</Text>
-				</Box>
-			)}
+          <Text
+            color="text100"
+            fontSize="small"
+            fontWeight="bold"
+            textAlign="left"
+          >
+            {formatUnits(BigInt(lowestListing.priceAmount), currency.decimals)}{" "}
+          </Text>
+        </Box>
+      )}
 
-			{balance && type !== ContractType.ERC721 && (
-				<Pill text={`Owned: ${balance}`} />
-			)}
+      {balanceOfCollectible && type !== ContractType.ERC721 && (
+        <Pill text={`Owned: ${balanceOfCollectible}`} />
+      )}
 
-			{type === ContractType.ERC721 && <Pill text="ERC-721" />}
-		</Box>
-	);
+      {type === ContractType.ERC721 && <Pill text="ERC-721" />}
+    </Box>
+  );
 };
