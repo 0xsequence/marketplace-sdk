95fb3ae0ed9bb1a727e4c476d22e87abead19a25
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-28 06:22:08 +0100

    Add useCancelOrder hook for order cancellation functionality

---
+++ packages/sdk/src/react/hooks/useCancelOrder.tsx
@@ -0,0 +1,33 @@
+import {
+  type TransactionConfig,
+  type CancelInput,
+  TransactionType,
+} from "../_internal/transaction-machine/execute-transaction";
+import { useTransactionMachine } from "../_internal/transaction-machine/useTransactionMachine";
+
+interface UseCancelOrderArgs
+  extends Omit<TransactionConfig, "sdkConfig" | "type"> {
+  onSuccess?: (hash: string) => void;
+  onError?: (error: Error) => void;
+}
+
+export const useCancelOrder = ({
+  onSuccess,
+  onError,
+  ...config
+}: UseCancelOrderArgs) => {
+  const machine = useTransactionMachine(
+    {
+      ...config,
+      type: TransactionType.CANCEL,
+    },
+    onSuccess,
+    onError
+  );
+
+  return {
+    cancel: (props: CancelInput) => machine?.start({ props }),
+    onError,
+    onSuccess,
+  };
+};
