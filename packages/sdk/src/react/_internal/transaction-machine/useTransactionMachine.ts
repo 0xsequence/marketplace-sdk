439155580a7821fbe7a4be67916e6386db50b021
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-12-01 08:45:55 +0100

    Refactor useTransactionMachine to include walletKind and improve type definitions

--- packages/sdk/src/react/_internal/transaction-machine/useTransactionMachine.ts
+++ packages/sdk/src/react/_internal/transaction-machine/useTransactionMachine.ts
@@ -1,50 +1,63 @@
-import { useSelectPaymentModal } from '@0xsequence/kit-checkout';
-import type { Hash } from 'viem';
-import { useWalletClient } from 'wagmi';
-import { getPublicRpcClient } from '../../../utils';
-import { useConfig, useMarketplaceConfig } from '../../hooks';
-import { useSwitchChainModal } from '../../ui/modals/_internal/components/switchChainModal';
+import { useSelectPaymentModal } from "@0xsequence/kit-checkout";
+import type { Hash } from "viem";
+import { useAccount, useWalletClient } from "wagmi";
+import { getPublicRpcClient } from "../../../utils";
+import { useConfig, useMarketplaceConfig } from "../../hooks";
+import { useSwitchChainModal } from "../../ui/modals/_internal/components/switchChainModal";
 import {
-	type TransactionConfig,
-	TransactionMachine,
-} from './execute-transaction';
+  type TransactionConfig,
+  TransactionMachine,
+} from "./execute-transaction";
+import { WalletKind } from "../api";
+
+export type UseTransactionMachineConfig = Omit<
+  TransactionConfig,
+  "sdkConfig" | "marketplaceConfig" | "walletKind"
+>;
 
 export const useTransactionMachine = (
-	config: Omit<TransactionConfig, 'sdkConfig' | 'marketplaceConfig'>,
-	onSuccess?: (hash: Hash) => void,
-	onError?: (error: Error) => void,
+  config: UseTransactionMachineConfig,
+  onSuccess?: (hash: Hash) => void,
+  onError?: (error: Error) => void
 ) => {
-	const { data: walletClient } = useWalletClient();
-	const { show: showSwitchChainModal } = useSwitchChainModal();
-	const sdkConfig = useConfig();
-	const { data: marketplaceConfig, error: marketplaceError } =
-		useMarketplaceConfig();
-	const { openSelectPaymentModal } = useSelectPaymentModal();
+  const { data: walletClient } = useWalletClient();
+  const { show: showSwitchChainModal } = useSwitchChainModal();
+  const sdkConfig = useConfig();
+  const { data: marketplaceConfig, error: marketplaceError } =
+    useMarketplaceConfig();
+  const { openSelectPaymentModal } = useSelectPaymentModal();
+  const { connector } = useAccount();
+  const walletKind =
+    connector?.id === "sequence" ? WalletKind.sequence : WalletKind.unknown;
 
-	if (marketplaceError) {
-		throw marketplaceError; //TODO: Add error handling
-	}
+  if (marketplaceError) {
+    throw marketplaceError; //TODO: Add error handling
+  }
 
-	if (!walletClient || !marketplaceConfig) return null;
+  if (!walletClient || !marketplaceConfig) return null;
 
-	return new TransactionMachine(
-		{ config: { sdkConfig, marketplaceConfig, ...config }, onSuccess, onError },
-		walletClient,
-		getPublicRpcClient(config.chainId),
-		openSelectPaymentModal,
-		async (chainId) => {
-			return new Promise<void>((resolve, reject) => {
-				showSwitchChainModal({
-					chainIdToSwitchTo: Number(chainId),
-					onSwitchChain: () => {
-						resolve();
-					},
-					callbacks: {
-						onUserRejectedRequest: () =>
-							reject(new Error('User rejected chain switch')),
-					},
-				});
-			});
-		},
-	);
+  return new TransactionMachine(
+    {
+      config: { sdkConfig, marketplaceConfig, walletKind, ...config },
+      onSuccess,
+      onError,
+    },
+    walletClient,
+    getPublicRpcClient(config.chainId),
+    openSelectPaymentModal,
+    async (chainId) => {
+      return new Promise<void>((resolve, reject) => {
+        showSwitchChainModal({
+          chainIdToSwitchTo: Number(chainId),
+          onSwitchChain: () => {
+            resolve();
+          },
+          callbacks: {
+            onUserRejectedRequest: () =>
+              reject(new Error("User rejected chain switch")),
+          },
+        });
+      });
+    }
+  );
 };
