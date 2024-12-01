627470df94718a1fe765dd9dfc41313e7d8bea76
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-12-01 10:40:04 +0100

    fix type definitions for useBuyCollectable and useCancelOrder

--- packages/sdk/src/react/hooks/useBuyCollectable.tsx
+++ packages/sdk/src/react/hooks/useBuyCollectable.tsx
@@ -1,33 +1,34 @@
 import {
-	type TransactionConfig,
-	type BuyInput,
-	TransactionType,
-} from '../_internal/transaction-machine/execute-transaction';
-import { useTransactionMachine } from '../_internal/transaction-machine/useTransactionMachine';
+  type BuyInput,
+  TransactionType,
+} from "../_internal/transaction-machine/execute-transaction";
+import {
+  useTransactionMachine,
+  type UseTransactionMachineConfig,
+} from "../_internal/transaction-machine/useTransactionMachine";
 
-interface UseBuyOrderArgs
-	extends Omit<TransactionConfig, 'sdkConfig' | 'type' | 'marketplaceConfig'> {
-	onSuccess?: (hash: string) => void;
-	onError?: (error: Error) => void;
+interface UseBuyOrderArgs extends UseTransactionMachineConfig {
+  onSuccess?: (hash: string) => void;
+  onError?: (error: Error) => void;
 }
 
 export const useBuyCollectable = ({
-	onSuccess,
-	onError,
-	...config
+  onSuccess,
+  onError,
+  ...config
 }: UseBuyOrderArgs) => {
-	const machine = useTransactionMachine(
-		{
-			...config,
-			type: TransactionType.BUY,
-		},
-		onSuccess,
-		onError,
-	);
+  const machine = useTransactionMachine(
+    {
+      ...config,
+      type: TransactionType.BUY,
+    },
+    onSuccess,
+    onError
+  );
 
-	return {
-		buy: (props: BuyInput) => machine?.start({ props }),
-		onError,
-		onSuccess,
-	};
+  return {
+    buy: (props: BuyInput) => machine?.start({ props }),
+    onError,
+    onSuccess,
+  };
 };
--- packages/sdk/src/react/hooks/useCancelOrder.tsx
+++ packages/sdk/src/react/hooks/useCancelOrder.tsx
@@ -1,33 +1,34 @@
 import {
-	type TransactionConfig,
-	type CancelInput,
-	TransactionType,
-} from '../_internal/transaction-machine/execute-transaction';
-import { useTransactionMachine } from '../_internal/transaction-machine/useTransactionMachine';
+  type CancelInput,
+  TransactionType,
+} from "../_internal/transaction-machine/execute-transaction";
+import {
+  useTransactionMachine,
+  type UseTransactionMachineConfig,
+} from "../_internal/transaction-machine/useTransactionMachine";
 
-interface UseCancelOrderArgs
-	extends Omit<TransactionConfig, 'sdkConfig' | 'type' | 'marketplaceConfig'> {
-	onSuccess?: (hash: string) => void;
-	onError?: (error: Error) => void;
+interface UseCancelOrderArgs extends UseTransactionMachineConfig {
+  onSuccess?: (hash: string) => void;
+  onError?: (error: Error) => void;
 }
 
 export const useCancelOrder = ({
-	onSuccess,
-	onError,
-	...config
+  onSuccess,
+  onError,
+  ...config
 }: UseCancelOrderArgs) => {
-	const machine = useTransactionMachine(
-		{
-			...config,
-			type: TransactionType.CANCEL,
-		},
-		onSuccess,
-		onError,
-	);
+  const machine = useTransactionMachine(
+    {
+      ...config,
+      type: TransactionType.CANCEL,
+    },
+    onSuccess,
+    onError
+  );
 
-	return {
-		cancel: (props: CancelInput) => machine?.start({ props }),
-		onError,
-		onSuccess,
-	};
+  return {
+    cancel: (props: CancelInput) => machine?.start({ props }),
+    onError,
+    onSuccess,
+  };
 };
