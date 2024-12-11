9db7dd2567141c10f347be685ef173c8afa10b56
Mithat Akbulut <marmarisco860@gmail.com>
2024-12-11 10:54:12 +0300

    apply new way of handling transactions, for cancel

--- packages/sdk/src/react/hooks/useCancel.tsx
+++ packages/sdk/src/react/hooks/useCancel.tsx
@@ -4,18 +4,28 @@ import {
 	TransactionType,
 } from '../_internal/transaction-machine/execute-transaction';
 import { ModalCallbacks } from '../ui/modals/_internal/types';
+import { StepType, Step, MarketplaceKind } from '../_internal';
+import { useEffect } from 'react';
 
 export default function useCancel({
 	collectionAddress,
 	chainId,
 	collectibleId,
+	orderId,
+	marketplace,
 	callbacks,
 }: {
 	collectionAddress: string;
 	chainId: string;
 	collectibleId: string;
+	orderId: string;
+	marketplace: MarketplaceKind;
 	callbacks: ModalCallbacks;
 }) {
+	const cancelProps = {
+		orderId,
+		marketplace,
+	} as CancelInput;
 	const machineConfig = {
 		type: TransactionType.CANCEL,
 		chainId: chainId,
@@ -28,17 +38,48 @@ export default function useCancel({
 		onError: callbacks.onError,
 	});
 
-	async function execute({ orderId, marketplace }: CancelInput) {
-		if (!machine?.transactionState?.transaction) return;
+	async function execute() {
+		if (!machine || !machine?.transactionState?.transaction.ready) return;
 
-		await machine?.transactionState?.transaction.execute({
-			type: TransactionType.CANCEL,
-			props: {
-				orderId,
-				marketplace: marketplace,
+		const steps = machine.transactionState.steps;
+
+		if (!steps.steps) {
+			throw new Error('Steps is undefined, cannot find execution step');
+		}
+
+		const executionStep = steps.steps.find(
+			(step) => step.id === StepType.cancel,
+		) as Step;
+
+		await machine.execute(
+			{
+				type: TransactionType.CANCEL,
+				props: cancelProps,
 			},
+			executionStep,
+		);
+	}
+
+	async function fetchSteps() {
+		if (
+			!machine ||
+			machine.transactionState === null ||
+			machine.transactionState.steps.checked
+		)
+			return;
+
+		await machine.fetchSteps({
+			type: TransactionType.CANCEL,
+			props: cancelProps,
 		});
 	}
+
+	// first time fetching steps
+	useEffect(() => {
+		if (!machine) return;
+
+		fetchSteps();
+	}, [machine]);
 
 	return {
 		execute,
