411acafef7bd7f82f6566fba7da273ccd9935c3f
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-12-13 07:13:50 +0100

    add waas

--- playgrounds/react-vite/vite.config.ts
+++ playgrounds/react-vite/vite.config.ts
@@ -5,5 +5,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
+  server:{
+    port: 4444
+  }
 });
