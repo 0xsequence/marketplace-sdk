5a721a4a7e10cffa38f4a8ed9103e329ede3a0c4
Mithat Akbulut <marmarisco860@gmail.com>
2024-12-16 23:06:13 +0300

    handle image loading error of currency

---
+++ packages/sdk/src/react/ui/modals/_internal/components/currencyImage/index.tsx
@@ -0,0 +1,53 @@
+import { Box, TokenImage } from '@0xsequence/design-system';
+import { useState } from 'react';
+import { Address } from 'viem';
+import { Price } from '../../../../../../types';
+import { Observable } from '@legendapp/state';
+
+function CurrencyImage({
+	$listingPrice,
+}: { $listingPrice: Observable<Price | undefined> }) {
+	const [imageLoadErrorCurrencyAddresses, setImageLoadErrorCurrencyAddresses] =
+		useState<Address[] | null>(null);
+
+	if (
+		imageLoadErrorCurrencyAddresses?.includes(
+			$listingPrice.currency.contractAddress.get() as Address,
+		)
+	) {
+		return (
+			<Box
+				width="3"
+				height="3"
+				borderRadius="circle"
+				background="backgroundSecondary"
+			/>
+		);
+	}
+
+	return (
+		<TokenImage
+			src={$listingPrice.currency.imageUrl.get()}
+			onError={() => {
+				const listingPrice = $listingPrice?.get();
+				if (listingPrice) {
+					setImageLoadErrorCurrencyAddresses((prev) => {
+						if (!prev)
+							return [listingPrice.currency.contractAddress as Address];
+						if (
+							!prev.includes(listingPrice.currency.contractAddress as Address)
+						) {
+							return [
+								...prev,
+								listingPrice.currency.contractAddress as Address,
+							];
+						}
+						return prev;
+					});
+				}
+			}}
+			size="xs"
+		/>
+	);
+}
+export default CurrencyImage;
