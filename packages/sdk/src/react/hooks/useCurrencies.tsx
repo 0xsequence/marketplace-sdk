382989bb3478e97942026c857597384d9a9a4c57
Mithat Akbulut <marmarisco860@gmail.com>
2024-12-17 16:15:38 +0300

    pass currency options to useCurrencies, instead of passing collectionAddress

--- packages/sdk/src/react/hooks/useCurrencies.tsx
+++ packages/sdk/src/react/hooks/useCurrencies.tsx
@@ -1,28 +1,25 @@
 import { queryOptions, useQuery } from '@tanstack/react-query';
 import { z } from 'zod';
-import type { MarketplaceConfig, SdkConfig } from '../../types';
-import { CollectionNotFoundError } from '../../utils/_internal/error/transaction';
+import type { SdkConfig } from '../../types';
+import { InvalidCurrencyOptionsError } from '../../utils/_internal/error/transaction';
 import {
 	AddressSchema,
 	type ChainId,
 	ChainIdSchema,
 	type Currency,
 	QueryArgSchema,
-	configKeys,
 	currencyKeys,
 	getMarketplaceClient,
-	getQueryClient,
 } from '../_internal';
 import { useConfig } from './useConfig';
-import { zeroAddress } from 'viem';
-import { useMarketplaceConfig } from './useMarketplaceConfig';
+import { toHex, zeroAddress } from 'viem';
 
 const ChainIdCoerce = ChainIdSchema.transform((val) => val.toString());
 
 const UseCurrenciesArgsSchema = z.object({
 	chainId: ChainIdCoerce,
-	collectionAddress: AddressSchema.optional(),
-	includeNativeCurrency: z.boolean().optional(),
+	includeNativeCurrency: z.boolean().optional().default(true),
+	currencyOptions: z.array(AddressSchema).optional(),
 	query: QueryArgSchema,
 });
 
@@ -42,46 +39,22 @@ const fetchCurrencies = async (chainId: ChainId, config: SdkConfig) => {
 	);
 };
 
-//TODO: Implement more proper way to get marketplace config
-const useControlledMarketplaceConfig = (projectId: number) => {
-	const { data: marketplaceConfig } = useMarketplaceConfig();
-	const queryClient = getQueryClient();
-	const marketplaceConfigCache = queryClient.getQueriesData({
-		queryKey: configKeys.marketplace,
-	})[0][1] as MarketplaceConfig;
-
-	if (marketplaceConfigCache.projectId !== projectId) {
-		return marketplaceConfig;
-	}
-
-	return marketplaceConfigCache;
-};
-
 const selectCurrencies = (
 	data: Currency[],
-	args: UseCurrenciesArgs,
-	marketplaceConfig: MarketplaceConfig | undefined,
+	args: UseCurrenciesArgs
 ) => {
 	console.debug('[selectCurrencies]: Select Currencies Input:', { data, args });
 	const argsParsed = UseCurrenciesArgsSchema.parse(args);
 	// if collectionAddress is passed, filter currencies based on collection currency options
-	console.debug('[selectCurrencies]: Select Currencies Args:', argsParsed);
-	if (argsParsed.collectionAddress) {
-		const collection = marketplaceConfig?.collections.find(
-			(collection) =>
-				collection.collectionAddress === argsParsed.collectionAddress,
-		);
-
-		console.debug('[selectCurrencies]: Collection:', collection);
-
-		if (!collection) {
-			throw new CollectionNotFoundError(argsParsed.collectionAddress!);
+	if (argsParsed.currencyOptions) {
+		if (!argsParsed.currencyOptions) {
+			throw new InvalidCurrencyOptionsError(argsParsed.currencyOptions);
 		}
 
 
 		return data.filter(
 			(currency) =>
-				collection.currencyOptions?.includes(currency.contractAddress) ||
+				argsParsed.currencyOptions?.includes(toHex(currency.contractAddress)) ||
 				// biome-ignore lint/suspicious/noDoubleEquals: <explanation>
 				currency.nativeCurrency == argsParsed.includeNativeCurrency ||
 				currency.defaultChainCurrency,
@@ -99,21 +72,18 @@ const selectCurrencies = (
 export const currenciesOptions = (
 	args: UseCurrenciesArgs,
 	config: SdkConfig,
-	marketplaceConfig: MarketplaceConfig | undefined,
 ) => {
 	return queryOptions({
 		...args.query,
 		queryKey: [...currencyKeys.lists, args.chainId],
 		queryFn: () => fetchCurrencies(args.chainId, config),
-		select: (data) => selectCurrencies(data, args, marketplaceConfig),
+		select: (data) => selectCurrencies(data, args),
 		enabled: args.query?.enabled,
 	});
 };
 
 export const useCurrencies = (args: UseCurrenciesArgs) => {
 	const config = useConfig();
-	const marketplaceConfig = useControlledMarketplaceConfig(
-		Number(config.projectId),
-	);
-	return useQuery(currenciesOptions(args, config, marketplaceConfig));
+
+	return useQuery(currenciesOptions(args, config));
 };
