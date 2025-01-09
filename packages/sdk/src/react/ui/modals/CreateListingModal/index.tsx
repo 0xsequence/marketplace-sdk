0c88d1c6c6e89f54f628bb3ae1420a473b15bc43
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-01-09 09:01:54 +0100

    Refactor store

--- packages/sdk/src/react/ui/modals/CreateListingModal/index.tsx
+++ packages/sdk/src/react/ui/modals/CreateListingModal/index.tsx
@@ -1,33 +1,12 @@
-import { Show } from '@legendapp/state/react';
-import type { Hash, Hex } from 'viem';
-import type { OrderbookKind } from '../../../_internal';
-import { useTransactionStatusModal } from '../_internal/components/transactionStatusModal';
 import type { ModalCallbacks } from '../_internal/types';
-import { createListingModal$ } from './_store';
-import { Modal } from './Modal';
+import { createListingModal$, OpenCreateListingModalArgs } from './store';
 
-export type ShowCreateListingModalArgs = {
-	collectionAddress: Hex;
-	chainId: string;
-	collectibleId: string;
-	orderbookKind: OrderbookKind;
-	onSuccess?: (hash?: Hash) => void;
-	onError?: (error: Error) => void;
-};
+type ShowCreateListingModalArgs = Exclude<OpenCreateListingModalArgs, 'callbacks'>;
 
 export const useCreateListingModal = (callbacks?: ModalCallbacks) => {
 	return {
 		show: (args: ShowCreateListingModalArgs) =>
-			createListingModal$.open({ ...args, defaultCallbacks: callbacks }),
+			createListingModal$.open({ ...args, callbacks }),
 		close: () => createListingModal$.close(),
 	};
-};
-
-export const CreateListingModal = () => {
-	const { show: showTransactionStatusModal } = useTransactionStatusModal();
-	return (
-		<Show if={createListingModal$.isOpen}>
-			<Modal showTransactionStatusModal={showTransactionStatusModal} />
-		</Show>
-	);
 };
--- packages/sdk/src/react/ui/modals/CreateListingModal/store.ts
+++ packages/sdk/src/react/ui/modals/CreateListingModal/store.ts
@@ -1,10 +1,43 @@
 import { observable } from '@legendapp/state';
 import { addDays } from 'date-fns/addDays';
-import type { Hash, Hex } from 'viem';
+import type { Hex } from 'viem';
 import { type Currency, OrderbookKind } from '../../../../types';
 import type { ModalCallbacks } from '../_internal/types';
+import { CollectionType } from '../../../_internal';
 
-const initialState = {
+type CreateListingState = {
+	isOpen: boolean;
+	collectionAddress: Hex;
+	chainId: string;
+	collectibleId: string;
+	collectionName: string;
+	orderbookKind: OrderbookKind;
+	collectionType: CollectionType | undefined;
+	listingPrice: {
+		amountRaw: string;
+		currency: Currency;
+	};
+	quantity: string;
+	invalidQuantity: boolean;
+	expiry: Date;
+	callbacks?: ModalCallbacks;
+};
+
+export type OpenCreateListingModalArgs = {
+	collectionAddress: Hex;
+	chainId: string;
+	collectibleId: string;
+	orderbookKind: OrderbookKind;
+	callbacks?: ModalCallbacks;
+	defaultCallbacks: ModalCallbacks;
+};
+
+type Actions = {
+	open: (args: OpenCreateListingModalArgs) => void;
+	close: () => void;
+};
+
+const initialState: CreateListingState = {
 	isOpen: false,
 	collectionAddress: '' as Hex,
 	chainId: '',
@@ -17,39 +50,28 @@ const initialState = {
 		amountRaw: '1',
 		currency: {} as Currency,
 	},
-	// to track if the user has changed the price, so we know if it's 1 default or user input
-	listingPriceChanged: false,
 	quantity: '1',
 	invalidQuantity: false,
 	expiry: new Date(addDays(new Date(), 7).toJSON()),
 	callbacks: undefined as ModalCallbacks | undefined,
+};
 
-	onError: undefined as undefined | ((error: Error) => void),
-	onSuccess: undefined as undefined | ((hash: Hash) => void),
-
-	open: (args: {
-		collectionAddress: Hex;
-		chainId: string;
-		collectibleId: string;
-		orderbookKind: OrderbookKind;
-		callbacks?: ModalCallbacks;
-		defaultCallbacks?: ModalCallbacks;
-		onSuccess?: (hash?: Hash) => void;
-		onError?: (error: Error) => void;
-	}) => {
+const actions: Actions = {
+	open: (args) => {
 		createListingModal$.collectionAddress.set(args.collectionAddress);
 		createListingModal$.chainId.set(args.chainId);
 		createListingModal$.collectibleId.set(args.collectibleId);
 		createListingModal$.orderbookKind.set(args.orderbookKind);
 		createListingModal$.callbacks.set(args.callbacks || args.defaultCallbacks);
-		createListingModal$.onSuccess.set(args.onSuccess);
-		createListingModal$.onError.set(args.onError);
 		createListingModal$.isOpen.set(true);
 	},
 	close: () => {
 		createListingModal$.isOpen.set(false);
-		createListingModal$.callbacks.set(undefined);
+		createListingModal$.set({ ...initialState, ...actions });
 	},
 };
 
-export const createListingModal$ = observable(initialState);
+export const createListingModal$ = observable<CreateListingState & Actions>({
+	...initialState,
+	...actions,
+});
