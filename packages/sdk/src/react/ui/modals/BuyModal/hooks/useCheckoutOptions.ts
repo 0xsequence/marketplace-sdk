69dbaf2fae5f7a876a7f8e201e67622721014615
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-01-13 12:25:50 +0100

    useCheckoutOptions

---
+++ packages/sdk/src/react/ui/modals/BuyModal/hooks/useCheckoutOptions.ts
@@ -0,0 +1,36 @@
+import { Hex } from "viem";
+import { getMarketplaceClient, MarketplaceKind } from "../../../../_internal";
+import { useConfig } from "../../../../hooks";
+import { useWallet } from "../../../../_internal/transaction-machine/useWallet";
+import { useFees } from "./useFees";
+import { useQuery } from "@tanstack/react-query";
+
+export const useCheckoutOptions = (input: {
+    chainId: number;
+    collectionAddress: Hex;
+    orderId: string;
+    marketplace: MarketplaceKind;
+}) => {
+    const config = useConfig();
+    const {wallet} = useWallet();
+    const fees = useFees({ chainId: input.chainId, collectionAddress: input.collectionAddress });
+
+    return useQuery({
+        queryKey: ['checkoutOptions', input.chainId, input.collectionAddress, input.orderId, input.marketplace],
+        queryFn: async () => {
+            const marketplaceClient = getMarketplaceClient(input.chainId, config);
+            return marketplaceClient.checkoutOptionsMarketplace({
+                wallet: await wallet!.address(),
+                orders: [
+                    {
+                        contractAddress: input.collectionAddress,
+                        orderId: input.orderId,
+                        marketplace: input.marketplace,
+                    },
+                ],
+                additionalFee: Number(fees.amount),
+            });
+        },
+        enabled: !!wallet
+    });
+};
