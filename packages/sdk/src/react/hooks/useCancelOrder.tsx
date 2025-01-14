d0b897b5b3fd925bee7dd5ed95bcb43c838a7751
Mithat Akbulut <marmarisco860@gmail.com>
2025-01-14 16:12:11 +0300

    check chain id and show switch chain modal if wallet is not waas, if it's waas just switch it

--- packages/sdk/src/react/hooks/useCancelOrder.tsx
+++ packages/sdk/src/react/hooks/useCancelOrder.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useCancelTransactionSteps } from './useCancelTransactionSteps';
-import { MarketplaceKind } from '../_internal';
+import { useSwitchChainModal } from '../ui/modals/_internal/components/switchChainModal';
+import { useWallet } from '../_internal/transaction-machine/useWallet';
+
 interface UseCancelOrderArgs {
 	collectionAddress: string;
 	chainId: string;
-	marketplace: MarketplaceKind;
-	orderId: string;
-	onSuccess?: ({hash, orderId}: {hash?: string, orderId?: string}) => void;
+	onSuccess?: ({ hash, orderId }: { hash?: string; orderId?: string }) => void;
 	onError?: (error: Error) => void;
 }
 
@@ -16,28 +16,98 @@ export type TransactionStep = {
 	execute: () => Promise<void>;
 };
 
+export type SwitchChainStatus = {
+	walletChainId: number | null;
+	mismatch: boolean;
+	isSwitching: boolean;
+};
+
 export const useCancelOrder = ({
 	collectionAddress,
 	chainId,
-	marketplace,
-	orderId,
 	onSuccess,
 	onError,
 }: UseCancelOrderArgs) => {
+	const { wallet } = useWallet();
+	const { show: showSwitchChainModal } = useSwitchChainModal();
 	const [steps, setSteps] = useState<TransactionStep>({
 		exist: false,
 		isExecuting: false,
 		execute: () => Promise.resolve(),
 	});
+	const [switchChainStatus, setSwitchChainStatus] = useState<SwitchChainStatus>(
+		{
+			walletChainId: null,
+			mismatch: false,
+			isSwitching: false,
+		},
+	);
+
+	const getWalletChainId = async () => {
+		return await wallet?.getChainId();
+	};
+	const switchChain = async () => {
+		if (!wallet) return;
+
+		await wallet.switchChain(Number(chainId));
+	};
+
+	useEffect(() => {
+		const checkAndSwitchChain = async () => {
+			const walletChainId = await getWalletChainId();
+			const isWaaS = wallet?.isWaaS;
+
+			if (walletChainId && walletChainId !== Number(chainId)) {
+				setSwitchChainStatus({
+					walletChainId,
+					mismatch: true,
+					isSwitching: false,
+				});
+
+				if (isWaaS) {
+					await switchChain();
+				} else {
+					showSwitchChainModal({
+						chainIdToSwitchTo: chainId,
+						onError: () => {
+							setSwitchChainStatus({
+								walletChainId,
+								mismatch: true,
+								isSwitching: false,
+							});
+						},
+						onClose: () => {
+							setSwitchChainStatus({
+								walletChainId,
+								mismatch: true,
+								isSwitching: false,
+							});
+						},
+						onSuccess: () => {
+							setSwitchChainStatus({
+								walletChainId,
+								mismatch: false,
+								isSwitching: false,
+							});
+						},
+					});
+				}
+			}
+		};
+
+		checkAndSwitchChain();
+	}, [chainId, wallet]);
 
 	const { cancelOrder } = useCancelTransactionSteps({
 		collectionAddress,
 		chainId,
-		marketplace,
-		orderId,
 		onSuccess,
 		onError,
 		setSteps,
+		disabled:
+			!wallet?.isWaaS &&
+			!!switchChainStatus.walletChainId &&
+			(switchChainStatus.mismatch || switchChainStatus.isSwitching),
 	});
 
 	return {
