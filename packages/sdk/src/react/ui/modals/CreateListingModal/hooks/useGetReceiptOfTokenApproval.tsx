50ff0c8ee160b8fa362bd0869cda53247eff9240
Mithat Akbulut <marmarisco860@gmail.com>
2025-01-14 11:15:57 +0300

    Add `useGetReceiptOfTokenApproval` hook for transaction receipt handling

---
+++ packages/sdk/src/react/ui/modals/CreateListingModal/hooks/useGetReceiptOfTokenApproval.tsx
@@ -0,0 +1,32 @@
+import { Hex } from 'viem';
+import { usePublicClient } from 'wagmi';
+import { useCallback } from 'react';
+
+/**
+ * @returns a function to wait for a transaction receipt and the receipt
+ * @description This hook is used to wait for a transaction receipt and the receipt
+ * @example
+ * const { waitForReceipt } = useGetReceiptOfTokenApproval();
+ * const receipt = await waitForReceipt(transactionHash);
+ */
+export const useGetReceiptOfTokenApproval = () => {
+	const publicClient = usePublicClient();
+
+	const waitForReceipt = useCallback(
+		async (transactionHash: Hex) => {
+			if (!publicClient) {
+				throw new Error('Public client not found');
+			}
+
+			const receipt = await publicClient.waitForTransactionReceipt({
+				hash: transactionHash,
+			});
+			return receipt;
+		},
+		[publicClient],
+	);
+
+	return {
+		waitForReceipt,
+	};
+};
