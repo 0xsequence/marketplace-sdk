e2be355eaf4d3bd18f86bd4912f916985920ded7
Mithat Akbulut <marmarisco860@gmail.com>
2025-01-22 19:03:16 +0300

    Add PriceCell component for displaying formatted price information

---
+++ playgrounds/react-vite/src/components/collectible/tables/PriceCell.tsx
@@ -0,0 +1,35 @@
+import { formatUnits } from "viem";
+import { Activity } from "../../../../../../packages/sdk/src";
+import { useCurrency } from "../../../../../../packages/sdk/src/react";
+import { useMarketplace } from "../../../lib/MarketplaceContext";
+import { Text } from "@0xsequence/design-system";
+
+
+const PriceCell = ({ activity }: { activity: Activity }) => {
+       const { chainId } = useMarketplace();
+       const { data: currency } = useCurrency({
+         chainId,
+         currencyAddress: activity.priceCurrencyAddress || "",
+         query: {
+           enabled: !!activity.priceCurrencyAddress,
+         },
+       });
+     
+       if (!activity.priceAmount || !activity.priceDecimals) {
+         return (
+           <Text fontFamily="body" color="text100">
+             -
+           </Text>
+         );
+       }
+     
+       return (
+         <Text fontFamily="body" color="text100">
+           {formatUnits(BigInt(activity.priceAmount), activity.priceDecimals)}{" "}
+           {currency?.symbol}
+         </Text>
+       );
+     };
+     
+
+export default PriceCell;
