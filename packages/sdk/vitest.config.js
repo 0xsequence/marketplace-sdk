4bc237d6e05f5940dbc6fba7a9042e89591e4e43
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-01-24 12:54:52 +0100

    vitest config

---
+++ packages/sdk/vitest.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+export default defineConfig({
+  plugins: [vanillaExtractPlugin()],
+	test: {
+		environment: "happy-dom",
+		include: ["./**/*.test.tsx"],
+	},
+});
