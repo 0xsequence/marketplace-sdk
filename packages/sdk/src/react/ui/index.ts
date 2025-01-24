2885a7359f8a62bb750b37d90813df8b8c5ae325
Mithat Akbulut <marmarisco860@gmail.com>
2025-01-24 11:59:46 +0300

    Refactor marketplace logo components: remove old logo exports, introduce lazy loading for improved performance, and restructure component organization.

--- packages/sdk/src/react/ui/index.ts
+++ packages/sdk/src/react/ui/index.ts
@@ -9,26 +9,3 @@ export { useBuyModal } from './modals/BuyModal';
 // components
 export { CollectibleCard } from './components/collectible-card/CollectibleCard';
 
-// Marketplace Logos
-export {
-	AlienSwapLogo,
-	AquaXyzLogo,
-	AuraLogo,
-	BlurLogo,
-	CoinbaseLogo,
-	ElementLogo,
-	FoundationLogo,
-	LooksRareLogo,
-	MagicEdenLogo,
-	ManifoldLogo,
-	MintifyLogo,
-	NftxLogo,
-	OkxLogo,
-	OpenSeaLogo,
-	RaribleLogo,
-	SequenceLogo,
-	SudoSwapLogo,
-	SuperRareLogo,
-	X2y2Logo,
-	ZoraLogo,
-} from './components/marketplace-logos';
