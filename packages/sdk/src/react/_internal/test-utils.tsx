68d6f8395f598d581a518faa806a732b055d4b8d
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-01-28 15:38:53 +0100

    add wagmi mock connectors

--- packages/sdk/src/react/_internal/test-utils.tsx
+++ packages/sdk/src/react/_internal/test-utils.tsx
@@ -2,6 +2,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render as rtlRender } from '@testing-library/react';
 import type { RenderOptions } from '@testing-library/react';
 import type { ReactElement } from 'react';
+import { createConfig, http, WagmiProvider } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { mock } from 'wagmi/connectors';
 
 const createTestQueryClient = () =>
 	new QueryClient({
@@ -13,16 +16,36 @@ const createTestQueryClient = () =>
 		},
 	});
 
+const config = createConfig({
+	chains: [mainnet, sepolia],
+	connectors: [
+		mock({
+			accounts: [
+				'0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+				'0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
+				'0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC',
+			],
+		}),
+	],
+	transports: {
+		[mainnet.id]: http(),
+		[sepolia.id]: http(),
+	},
+});
+
 export function renderWithClient(
 	ui: ReactElement,
 	options?: Omit<RenderOptions, 'wrapper'>,
 ) {
 	const testQueryClient = createTestQueryClient();
+
 	const { rerender, ...result } = rtlRender(ui, {
 		wrapper: ({ children }) => (
-			<QueryClientProvider client={testQueryClient}>
-				{children}
-			</QueryClientProvider>
+			<WagmiProvider config={config}>
+				<QueryClientProvider client={testQueryClient}>
+					{children}
+				</QueryClientProvider>
+			</WagmiProvider>
 		),
 		...options,
 	});
@@ -31,13 +54,13 @@ export function renderWithClient(
 		...result,
 		rerender: (rerenderUi: ReactElement) =>
 			rerender(
-				<QueryClientProvider client={testQueryClient}>
-					{rerenderUi}
-				</QueryClientProvider>,
+				<WagmiProvider config={config}>
+					<QueryClientProvider client={testQueryClient}>
+						{rerenderUi}
+					</QueryClientProvider>
+				</WagmiProvider>,
 			),
 	};
 }
 
 export * from '@testing-library/react';
-
-export { renderWithClient as render };
