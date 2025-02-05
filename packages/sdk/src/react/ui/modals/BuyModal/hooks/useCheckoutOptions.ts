9d974d7686bb6fcfb5791d11985b8c04bfaa9e43
Mithat Akbulut <marmarisco860@gmail.com>
2025-02-05 15:17:51 +0300

    fix: update useCheckoutOptions to handle missing wallet case with skipToken

--- packages/sdk/src/react/ui/modals/BuyModal/hooks/useCheckoutOptions.ts
+++ packages/sdk/src/react/ui/modals/BuyModal/hooks/useCheckoutOptions.ts
@@ -6,7 +6,7 @@ import {
 import { useConfig } from '../../../../hooks';
 import { useWallet } from '../../../../_internal/wallet/useWallet';
 import { useFees } from './useFees';
-import { useQuery } from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 
 export const useCheckoutOptions = (input: {
 	chainId: number;
@@ -29,22 +29,24 @@ export const useCheckoutOptions = (input: {
 			input.orderId,
 			input.marketplace,
 		],
-		queryFn: async () => {
-			const marketplaceClient = getMarketplaceClient(input.chainId, config);
-			return marketplaceClient
-				.checkoutOptionsMarketplace({
-					wallet: await wallet!.address(),
-					orders: [
-						{
-							contractAddress: input.collectionAddress,
-							orderId: input.orderId,
-							marketplace: input.marketplace,
-						},
-					],
-					additionalFee: Number(fees.amount),
-				})
-				.then((res) => res.options);
-		},
+		queryFn: wallet
+			? async () => {
+					const marketplaceClient = getMarketplaceClient(input.chainId, config);
+					return marketplaceClient
+						.checkoutOptionsMarketplace({
+							wallet: await wallet.address(),
+							orders: [
+								{
+									contractAddress: input.collectionAddress,
+									orderId: input.orderId,
+									marketplace: input.marketplace,
+								},
+							],
+							additionalFee: Number(fees.amount),
+						})
+						.then((res) => res.options);
+				}
+			: skipToken,
 		enabled: !!wallet,
 	});
 };
