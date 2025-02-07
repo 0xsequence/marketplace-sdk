76d72b1237a3f625511a5e869bc28a1973a16af9
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-02-07 15:38:31 +0100

    improve query key

--- packages/sdk/src/react/_internal/wallet/useWallet.ts
+++ packages/sdk/src/react/_internal/wallet/useWallet.ts
@@ -1,10 +1,10 @@
-import { useAccount, useSwitchChain, useWalletClient } from 'wagmi';
+import { useAccount, useChainId, useSwitchChain, useWalletClient } from 'wagmi';
 import { useQuery } from '@tanstack/react-query';
 import { wallet, type WalletInstance } from './wallet';
 import { useConfig } from '../../hooks';
 
 type UseWalletReturn = {
-  data: WalletInstance | null | undefined;
+  wallet: WalletInstance | null | undefined;
   isLoading: boolean;
   isError: boolean;
 };
@@ -14,9 +14,10 @@ export const useWallet = (): UseWalletReturn => {
   const { data: walletClient, isLoading: wagmiWalletIsLoading } = useWalletClient();
   const { connector, isConnected, isConnecting } = useAccount();
   const sdkConfig = useConfig();
+  const chainId = useChainId()
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['wallet', walletClient?.account.address, connector?.id],
+    queryKey: ['wallet', chainId, connector?.uid],
     queryFn: () => {
       if (!walletClient || !connector || !isConnected) {
         return null;
@@ -28,11 +29,12 @@ export const useWallet = (): UseWalletReturn => {
         sdkConfig,
       });
     },
+    staleTime: Number.POSITIVE_INFINITY,
     enabled: Boolean(walletClient && connector && isConnected),
   });
 
   return {
-    data,
+    wallet: data,
     isLoading: isLoading || isConnecting || wagmiWalletIsLoading,
     isError,
   };
