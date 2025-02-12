d98f14473c530a48c74b916c0e5fbb40502cd9d1
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-02-12 08:10:09 +0100

    convert table to tailwind

--- playgrounds/react-vite/src/lib/Table/Table.tsx
+++ playgrounds/react-vite/src/lib/Table/Table.tsx
@@ -1,35 +1,46 @@
 import * as React from 'react';
-import { styles } from './table.css';
 
 const Table = React.forwardRef<
 	HTMLTableElement,
 	React.HTMLAttributes<HTMLTableElement>
->(({ ...props }, ref) => (
-	<table ref={ref} className={styles.table} {...props} />
+>(({ className, ...props }, ref) => (
+	<table
+		ref={ref}
+		className={`w-full text-sm caption-bottom ${className || ''}`}
+		{...props}
+	/>
 ));
 Table.displayName = 'Table';
 
 const TableHeader = React.forwardRef<
 	HTMLTableSectionElement,
 	React.HTMLAttributes<HTMLTableSectionElement>
->(({ ...props }, ref) => (
-	<thead ref={ref} className={styles.tableHeader} {...props} />
+>(({ className, ...props }, ref) => (
+	<thead
+		ref={ref}
+		className={`bg-foreground/10 border-b border-border ${className || ''}`}
+		{...props}
+	/>
 ));
 TableHeader.displayName = 'TableHeader';
 
 const TableBody = React.forwardRef<
 	HTMLTableSectionElement,
 	React.HTMLAttributes<HTMLTableSectionElement>
->(({ ...props }, ref) => (
-	<tbody ref={ref} className={styles.tableBody} {...props} />
+>(({ className, ...props }, ref) => (
+	<tbody ref={ref} className={`border-b-0 ${className || ''}`} {...props} />
 ));
 TableBody.displayName = 'TableBody';
 
 const TableFooter = React.forwardRef<
 	HTMLTableSectionElement,
 	React.HTMLAttributes<HTMLTableSectionElement>
->(({ ...props }, ref) => (
-	<tfoot ref={ref} className={styles.tableFooter} {...props} />
+>(({ className, ...props }, ref) => (
+	<tfoot
+		ref={ref}
+		className={`border-t border-foreground/10 ${className || ''}`}
+		{...props}
+	/>
 ));
 TableFooter.displayName = 'TableFooter';
 
@@ -37,31 +48,47 @@ const TableRow = React.forwardRef<
 	HTMLTableRowElement,
 	React.HTMLAttributes<HTMLTableRowElement>
 >(({ className, ...props }, ref) => (
-	<tr ref={ref} className={`${styles.tableRow} ${className}`} {...props} />
+	<tr
+		ref={ref}
+		className={`border-b border-border transition-colors duration-300 hover:bg-foreground/[0.01] data-[state=selected]:bg-accent/40 data-[state=selected]:nth-odd:bg-accent/40 ${className || ''}`}
+		{...props}
+	/>
 ));
 TableRow.displayName = 'TableRow';
 
 const TableHead = React.forwardRef<
 	HTMLTableCellElement,
 	React.ThHTMLAttributes<HTMLTableCellElement>
->(({ ...props }, ref) => (
-	<th ref={ref} className={styles.tableHead} {...props} />
+>(({ className, ...props }, ref) => (
+	<th
+		ref={ref}
+		className={`h-12 pl-4 pr-0 text-left align-middle font-medium text-foreground md:h-auto md:p-1 ${className || ''}`}
+		{...props}
+	/>
 ));
 TableHead.displayName = 'TableHead';
 
 const TableCell = React.forwardRef<
 	HTMLTableCellElement,
 	React.TdHTMLAttributes<HTMLTableCellElement>
->(({ ...props }, ref) => (
-	<td ref={ref} className={styles.tableCell} {...props} />
+>(({ className, ...props }, ref) => (
+	<td
+		ref={ref}
+		className={`p-4 align-middle has-[role=checkbox]:pr-0 ${className || ''}`}
+		{...props}
+	/>
 ));
 TableCell.displayName = 'TableCell';
 
 const TableCaption = React.forwardRef<
 	HTMLTableCaptionElement,
 	React.HTMLAttributes<HTMLTableCaptionElement>
->(({ ...props }, ref) => (
-	<caption ref={ref} className={styles.tableCaption} {...props} />
+>(({ className, ...props }, ref) => (
+	<caption
+		ref={ref}
+		className={`mt-4 text-sm text-muted-foreground ${className || ''}`}
+		{...props}
+	/>
 ));
 TableCaption.displayName = 'TableCaption';
 
