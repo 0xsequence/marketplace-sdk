a9a27d9d76362159ba71f841204a4d6cbb9f655c
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-02-12 07:38:26 +0100

    add tailwind

--- playgrounds/react-vite/vite.config.ts
+++ playgrounds/react-vite/vite.config.ts
@@ -2,10 +2,12 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
+	plugins: [react(), tsconfigPaths(), tailwindcss(), vanillaExtractPlugin()],
 	server: {
 		port: 4444,
 	},
