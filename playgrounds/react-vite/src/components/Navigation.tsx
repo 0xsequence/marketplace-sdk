2c82b133f526510ac3b951ba08268da6a050e586
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-02-17 16:22:03 +0100

    fix navbar

--- playgrounds/react-vite/src/components/Navigation.tsx
+++ playgrounds/react-vite/src/components/Navigation.tsx
@@ -1,21 +1,22 @@
-import { useNavigate, useLocation } from 'react-router';
-import { ROUTES } from '../lib/routes';
+import { useNavigate, useLocation } from "react-router";
+import { ROUTES } from "../lib/routes";
+import { Button } from "@0xsequence/design-system2";
 
 export function Navigation() {
-	const navigate = useNavigate();
-	const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	return (
-		<div className="flex gap-3 flex-row bg-background-backdrop p-3 rounded-xl">
-			{Object.values(ROUTES).map(({ label, path }) => (
-				<button
-					className="p-3 rounded-xl grow text-center border-none text-text100 font-bold cursor-pointer"
-					key={path}
-					onClick={() => navigate(`/${path}`)}
-				>
-					{label}
-				</button>
-			))}
-		</div>
-	);
+  return (
+    <div className="flex gap-3 flex-row bg-background-backdrop p-3 rounded-xl">
+      {Object.values(ROUTES).map(({ label, path }) => (
+        <Button
+          variant={location.pathname === `/${path}` ? "primary" : "secondary"}
+          key={path}
+          onClick={() => navigate(`/${path}`)}
+        >
+          {label}
+        </Button>
+      ))}
+    </div>
+  );
 }
