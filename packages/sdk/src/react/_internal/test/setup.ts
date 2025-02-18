2ba8df48629f6348f1512a714e19a942a46e0656
Mithat Akbulut <marmarisco860@gmail.com>
2025-02-18 14:48:28 +0300

    feat(mocks): add MSW handlers for indexer API with mock data

--- packages/sdk/src/react/_internal/test/setup.ts
+++ packages/sdk/src/react/_internal/test/setup.ts
@@ -3,8 +3,13 @@ import { cleanup } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { handlers as marketplaceHandlers } from '../api/__mocks__/marketplace.msw';
 import { handlers as metadataHandlers } from '../api/__mocks__/metadata.msw';
+import { handlers as indexerHandlers } from '../api/__mocks__/indexer.msw';
 
-export const server = setupServer(...marketplaceHandlers, ...metadataHandlers);
+export const server = setupServer(
+	...marketplaceHandlers,
+	...metadataHandlers,
+	...indexerHandlers,
+);
 
 beforeAll(() => {
 	server.listen();
