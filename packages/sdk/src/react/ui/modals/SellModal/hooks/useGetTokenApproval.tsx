ff772af90e5d9b0ab60631e9be0a1b4120f791aa
Mithat Akbulut <marmarisco860@gmail.com>
2025-02-19 15:25:03 +0300

    feat: update useGetTokenApprovalData to conditionally skip query when wallet is not available

--- packages/sdk/src/react/ui/modals/SellModal/hooks/useGetTokenApproval.tsx
+++ packages/sdk/src/react/ui/modals/SellModal/hooks/useGetTokenApproval.tsx
@@ -5,7 +5,7 @@ import {
 	type OrderData,
 	StepType,
 } from '../../../../_internal';
-import { useQuery } from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 import { useWallet } from '../../../../_internal/wallet/useWallet';
 import { useConfig } from '../../../../hooks/useConfig';
 import { useFees } from '../../BuyModal/hooks/useFees';
@@ -30,38 +30,40 @@ export const useGetTokenApprovalData = (
 
 	const { data, isLoading, isSuccess } = useQuery({
 		queryKey: ['token-approval-data', params.ordersData],
-		queryFn: async () => {
-			const address = await wallet!.address();
-			const args = {
-				collectionAddress: params.collectionAddress,
-				walletType: wallet!.walletKind,
-				seller: address,
-				marketplace: params.marketplace,
-				ordersData: params.ordersData,
-				additionalFees: [
-					{
-						amount,
-						receiver,
-					},
-				],
-			} satisfies GenerateSellTransactionArgs;
-			const steps = await marketplaceClient
-				.generateSellTransaction(args)
-				.then((resp) => resp.steps);
+		queryFn: wallet
+			? async () => {
+					const address = await wallet.address();
+					const args = {
+						collectionAddress: params.collectionAddress,
+						walletType: wallet.walletKind,
+						seller: address,
+						marketplace: params.marketplace,
+						ordersData: params.ordersData,
+						additionalFees: [
+							{
+								amount,
+								receiver,
+							},
+						],
+					} satisfies GenerateSellTransactionArgs;
+					const steps = await marketplaceClient
+						.generateSellTransaction(args)
+						.then((resp) => resp.steps);
 
-			const tokenApprovalStep = steps.find(
-				(step) => step.id === StepType.tokenApproval,
-			);
-			if (!tokenApprovalStep) {
-				return {
-					step: null,
-				};
-			}
+					const tokenApprovalStep = steps.find(
+						(step) => step.id === StepType.tokenApproval,
+					);
+					if (!tokenApprovalStep) {
+						return {
+							step: null,
+						};
+					}
 
-			return {
-				step: tokenApprovalStep,
-			};
-		},
+					return {
+						step: tokenApprovalStep,
+					};
+				}
+			: skipToken,
 		enabled: !!wallet && !!params.collectionAddress,
 	});
 
