88716b9d81ee7e6eaf6a6f8e7f56e1ecb5178b01
Mithat Akbulut <marmarisco860@gmail.com>
2025-03-07 14:34:20 +0300

    create barrel file and export the options

--- packages/sdk/src/react/index.ts
+++ packages/sdk/src/react/index.ts
@@ -1,5 +1,6 @@
 export * from './provider';
 export * from './hooks';
+export * from './hooks/options';
 export * from './_internal/api/get-query-client';
 export * from './_internal/wagmi/create-config';
 export * from './ui/index';
