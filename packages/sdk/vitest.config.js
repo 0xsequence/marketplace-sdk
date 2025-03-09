b08f604c8afb59c3e2ced32d44bdafd4c74f070d
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-03-09 20:54:24 +0100

    add prool

--- packages/sdk/vitest.config.js
+++ packages/sdk/vitest.config.js
@@ -1,11 +1,12 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [vanillaExtractPlugin()],
+	plugins: [vanillaExtractPlugin(), tsconfigPaths()],
 	test: {
 		environment: 'jsdom',
-		setupFiles: ['./test/setup.ts'],
+		setupFiles: ['./src/test/setup.ts'],
 		include: ['./**/*.test.{ts,tsx}'],
 	},
 });
