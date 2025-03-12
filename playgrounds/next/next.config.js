c782e481f1970162f970854bea6ae5cd80897bee
Mithat Akbulut <marmarisco860@gmail.com>
2025-03-12 11:58:50 +0300

    chore: clean up Next.js playground dependencies and configuration

--- playgrounds/next/next.config.js
+++ playgrounds/next/next.config.js
@@ -3,6 +3,9 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	transpilePackages: ['@0xsequence/marketplace-sdk'],
 };
 
