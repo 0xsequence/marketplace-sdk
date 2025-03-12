7ec15519877b9ef911105aa2cb56c5d382421875
Mithat Akbulut <marmarisco860@gmail.com>
2025-03-12 12:24:38 +0300

    chore: remove Vanilla Extract plugin from Next.js configuration

--- playgrounds/next/next.config.js
+++ playgrounds/next/next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
-const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
 	eslint: {
@@ -9,4 +7,4 @@ const nextConfig = {
 	transpilePackages: ['@0xsequence/marketplace-sdk'],
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = nextConfig;
