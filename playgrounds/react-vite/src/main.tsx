8b0abdb9d2e0f44a4d3fae683c5fce4b5ee93fc9
Mithat Akbulut <marmarisco860@gmail.com>
2025-03-12 12:27:51 +0300

    remove debug tab

--- playgrounds/react-vite/src/main.tsx
+++ playgrounds/react-vite/src/main.tsx
@@ -8,7 +8,6 @@ import { DEFAULT_ROUTE, ROUTES } from './lib/routes';
 import { Collectible } from './tabs/Collectable';
 import { Collectibles } from './tabs/Collectables';
 import { Collections } from './tabs/Collections';
-import { Debug } from './tabs/Debug';
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
@@ -21,7 +20,6 @@ createRoot(document.getElementById('root')!).render(
 						<Route path={ROUTES.COLLECTIONS.path} element={<Collections />} />
 						<Route path={ROUTES.COLLECTIBLES.path} element={<Collectibles />} />
 						<Route path={ROUTES.COLLECTIBLE.path} element={<Collectible />} />
-						<Route path={ROUTES.DEBUG.path} element={<Debug />} />
 					</Route>
 				</Routes>
 			</Providers>
