013d824fa15b6eda708100d4d9709d58785af76d
Mithat Akbulut <marmarisco860@gmail.com>
2025-03-17 17:48:14 +0300

    remove components of shared/table

--- playgrounds/shared/src/components/table/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Table';
-export * from './ControlledTable';
-export * from './ActionCell';
-export * from './CurrencyCell';
-export * from './ListingsTable';
-export * from './OffersTable';
-export * from './ActivitiesTable';
