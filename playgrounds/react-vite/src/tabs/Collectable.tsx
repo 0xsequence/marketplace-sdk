f89f42be8a4dab95eedcb51c80c41431fe76d792
Mithat Akbulut <marmarisco860@gmail.com>
2025-03-17 18:12:59 +0300

    remove restriction on seeing collectible page without connecting wallet

--- playgrounds/react-vite/src/tabs/Collectable.tsx
+++ playgrounds/react-vite/src/tabs/Collectable.tsx
@@ -1,99 +1,95 @@
-import { type ContractType, OrderSide } from '@0xsequence/marketplace-sdk';
+import { type ContractType, OrderSide } from "@0xsequence/marketplace-sdk";
 import {
-	CollectibleCard,
-	useBalanceOfCollectible,
-	useCollectible,
-	useCollection,
-	useListCollectibles,
-	useLowestListing,
-} from '@0xsequence/marketplace-sdk/react';
-import { Actions, useMarketplace } from 'shared-components';
-import { useAccount } from 'wagmi';
-import { CollectibleDetails } from '../components/collectible';
-import ListingsTable from 'shared-components/src/components/ordersTable/ListingsTable';
+  CollectibleCard,
+  useBalanceOfCollectible,
+  useCollectible,
+  useCollection,
+  useListCollectibles,
+  useLowestListing,
+} from "@0xsequence/marketplace-sdk/react";
+import { Actions, useMarketplace } from "shared-components";
+import { useAccount } from "wagmi";
+import { CollectibleDetails } from "../components/collectible";
+import ListingsTable from "shared-components/src/components/ordersTable/ListingsTable";
 
 export function Collectible() {
-	const context = useMarketplace();
-	const { address: accountAddress, chainId: accountChainId } = useAccount();
-	const { collectionAddress, chainId, collectibleId } = context;
-	const { data: collectible, isLoading: collectibleLoading } = useCollectible({
-		collectionAddress,
-		chainId,
-		collectibleId,
-	});
-	const { data: filteredCollectibles, isLoading: filteredCollectiblesLoading } =
-		useListCollectibles({
-			collectionAddress,
-			chainId,
-			side: OrderSide.listing,
-			filter: {
-				includeEmpty: true,
-				searchText: collectible?.name,
-			},
-		});
-	const { data: lowestListing } = useLowestListing({
-		collectionAddress,
-		chainId,
-		tokenId: collectibleId,
-	});
-	const { data: collection, isLoading: collectionLoading } = useCollection({
-		collectionAddress,
-		chainId,
-	});
-	const { data: balance } = useBalanceOfCollectible({
-		collectionAddress,
-		chainId,
-		collectableId: collectibleId,
-		userAddress: accountAddress,
-	});
+  const context = useMarketplace();
+  const { address: accountAddress } = useAccount();
+  const { collectionAddress, chainId, collectibleId } = context;
+  const { data: collectible, isLoading: collectibleLoading } = useCollectible({
+    collectionAddress,
+    chainId,
+    collectibleId,
+  });
+  const { data: filteredCollectibles, isLoading: filteredCollectiblesLoading } =
+    useListCollectibles({
+      collectionAddress,
+      chainId,
+      side: OrderSide.listing,
+      filter: {
+        includeEmpty: true,
+        searchText: collectible?.name,
+      },
+    });
+  const { data: lowestListing } = useLowestListing({
+    collectionAddress,
+    chainId,
+    tokenId: collectibleId,
+  });
+  const { data: collection, isLoading: collectionLoading } = useCollection({
+    collectionAddress,
+    chainId,
+  });
+  const { data: balance } = useBalanceOfCollectible({
+    collectionAddress,
+    chainId,
+    collectableId: collectibleId,
+    userAddress: accountAddress,
+  });
 
-	const filteredCollectible = filteredCollectibles?.pages[0].collectibles.find(
-		(fc) => fc.metadata.tokenId === collectibleId,
-	);
-	const balanceString = balance?.balance?.toString();
+  const filteredCollectible = filteredCollectibles?.pages[0].collectibles.find(
+    (fc) => fc.metadata.tokenId === collectibleId
+  );
+  const balanceString = balance?.balance?.toString();
 
-	if (!accountChainId) {
-		return <div>Please connect to the correct chain</div>;
-	}
+  return (
+    <div className="flex flex-col gap-3 pt-3">
+      <div className="flex gap-3">
+        <CollectibleCard
+          collectibleId={collectibleId}
+          chainId={chainId}
+          collectionAddress={collectionAddress}
+          orderbookKind={context.orderbookKind}
+          collectionType={collection?.type as ContractType}
+          lowestListing={filteredCollectible}
+          onOfferClick={({ order }) => console.log(order)}
+          balance={balanceString}
+          cardLoading={
+            collectibleLoading ||
+            filteredCollectiblesLoading ||
+            collectionLoading
+          }
+        />
 
-	return (
-		<div className="flex flex-col gap-3 pt-3">
-			<div className="flex gap-3">
-				<CollectibleCard
-					collectibleId={collectibleId}
-					chainId={chainId}
-					collectionAddress={collectionAddress}
-					orderbookKind={context.orderbookKind}
-					collectionType={collection?.type as ContractType}
-					lowestListing={filteredCollectible}
-					onOfferClick={({ order }) => console.log(order)}
-					balance={balanceString}
-					cardLoading={
-						collectibleLoading ||
-						filteredCollectiblesLoading ||
-						collectionLoading
-					}
-				/>
-
-				<CollectibleDetails
-					name={collectible?.name}
-					id={collectibleId.toString()}
-					balance={Number(balance?.balance)}
-				/>
-			</div>
-			<Actions
-				isOwner={!!balance?.balance}
-				collectionAddress={collectionAddress}
-				chainId={chainId}
-				collectibleId={collectibleId}
-				orderbookKind={context.orderbookKind}
-				lowestListing={lowestListing?.order}
-			/>
-			<ListingsTable
-				chainId={chainId}
-				collectionAddress={collectionAddress}
-				collectibleId={collectibleId.toString()}
-			/>
-		</div>
-	);
+        <CollectibleDetails
+          name={collectible?.name}
+          id={collectibleId.toString()}
+          balance={Number(balance?.balance)}
+        />
+      </div>
+      <Actions
+        isOwner={!!balance?.balance}
+        collectionAddress={collectionAddress}
+        chainId={chainId}
+        collectibleId={collectibleId}
+        orderbookKind={context.orderbookKind}
+        lowestListing={lowestListing?.order}
+      />
+      <ListingsTable
+        chainId={chainId}
+        collectionAddress={collectionAddress}
+        collectibleId={collectibleId.toString()}
+      />
+    </div>
+  );
 }
