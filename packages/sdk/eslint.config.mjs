b2f0e987ff4897e1c627d0ee7c53829cf986b01a
Alexander Kolberg <74478295+AlexanderKolberg@users.noreply.github.com>
2025-03-19 09:25:02 +0100

    Update packages/sdk/eslint.config.mjs
    Co-authored-by: Mithat Akbulut <marmarisco860@gmail.com>

--- packages/sdk/eslint.config.mjs
+++ packages/sdk/eslint.config.mjs
@@ -9,7 +9,7 @@ import rscPlugin from './eslint/use-client.js';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+{languageOptions: { globals: { ...globals.browser, ...globals.node } },  settings: { react: { version: "detect"} } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
 	pluginReact.configs.flat.recommended,
