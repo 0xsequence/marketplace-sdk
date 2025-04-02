155a636714f0df4f8deba2cfc04a5945e873f564
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-04-02 17:08:12 +0200

    buy modal index

--- sdk/src/react/ui/modals/BuyModal/index.tsx
+++ sdk/src/react/ui/modals/BuyModal/index.tsx
@@ -1,20 +1,10 @@
-import type { Hex } from 'viem';
-import type { Order } from '../../../_internal';
 import type { ModalCallbacks } from '../_internal/types';
-import { buyModal$ } from './store';
-
-export type ShowBuyModalArgs = {
-	chainId: string;
-	collectionAddress: Hex;
-	tokenId: string;
-	order: Order;
-	customProviderCallback?: (args: { data: string; value: string }) => void;
-};
+import { type BuyModalProps, buyModalStore } from './store';
 
 export const useBuyModal = (callbacks?: ModalCallbacks) => {
 	return {
-		show: (args: ShowBuyModalArgs) =>
-			buyModal$.open({ ...args, defaultCallbacks: callbacks }),
-		close: () => buyModal$.close(),
+		show: (args: BuyModalProps) =>
+			buyModalStore.send({ type: 'open', props: args, ...callbacks }),
+		close: () => buyModalStore.send({ type: 'close' }),
 	};
 };
