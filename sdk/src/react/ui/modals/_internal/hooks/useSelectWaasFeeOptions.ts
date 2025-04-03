f8a5b960cb7fbc4dc195295b9da18a29b323bff8
Mithat Akbulut <marmarisco860@gmail.com>
2025-04-03 14:46:13 +0300

    feat: Add custom hook useSelectWaasFeeOptions for managing WaaS fee options visibility and action handling. This hook integrates wallet state and network type to conditionally display fee options and manage processing states effectively.

---
+++ sdk/src/react/ui/modals/_internal/hooks/useSelectWaasFeeOptions.ts
@@ -0,0 +1,61 @@
+import { NetworkType } from '@0xSequence/network';
+import { getNetwork } from '@0xsequence/connect';
+import { useWallet } from '../../../../_internal/wallet/useWallet';
+import { selectWaasFeeOptions$ } from '../components/selectWaasFeeOptions/store';
+
+interface UseSelectWaasFeeOptionsProps {
+	chainId: string | number;
+	isProcessing: boolean;
+}
+
+export const useSelectWaasFeeOptions = ({
+	chainId,
+	isProcessing,
+}: UseSelectWaasFeeOptionsProps) => {
+	const { wallet } = useWallet();
+	const feeOptionsVisible = selectWaasFeeOptions$.isVisible.get();
+	const network = chainId ? getNetwork(Number(chainId)) : undefined;
+	const isTestnet = network?.type === NetworkType.TESTNET;
+	const isWaaS = wallet?.isWaaS;
+	const isProcessingWithWaaS = isProcessing && isWaaS;
+	const selectedFeeOption = selectWaasFeeOptions$.selectedFeeOption.get();
+
+	const shouldHideActionButton =
+		!isTestnet &&
+		isProcessingWithWaaS &&
+		feeOptionsVisible === true &&
+		!!selectedFeeOption;
+
+	const waasFeeOptionsShown =
+		wallet?.isWaaS && isProcessing && feeOptionsVisible;
+
+	const getActionLabel = (
+		defaultLabel: string,
+		loadingLabel = 'Loading fee options',
+	) => {
+		if (isProcessing) {
+			return isWaaS && !isTestnet ? loadingLabel : defaultLabel;
+		}
+		return defaultLabel;
+	};
+
+	const showWaasFeeOptions = () => {
+		selectWaasFeeOptions$.isVisible.set(true);
+	};
+
+	const hideWaasFeeOptions = () => {
+		selectWaasFeeOptions$.hide();
+	};
+
+	return {
+		isWaaS,
+		isTestnet,
+		feeOptionsVisible,
+		shouldHideActionButton,
+		waasFeeOptionsShown,
+		isProcessingWithWaaS,
+		getActionLabel,
+		showWaasFeeOptions,
+		hideWaasFeeOptions,
+	};
+};
