96367765beb5c55a4bc4ad5a3dcab0d01c963864
Mithat Akbulut <marmarisco860@gmail.com>
2025-04-22 11:22:32 +0300

    Refactor FiltersSidebar to utilize useFiltersProgressive hook, removing unnecessary state management and effects

--- playgrounds/shared/src/components/filters/FiltersSidebar.tsx
+++ playgrounds/shared/src/components/filters/FiltersSidebar.tsx
@@ -1,7 +1,6 @@
 import { Scroll, Text } from '@0xsequence/design-system';
-import { useEffect, useState } from 'react';
 import type { Address } from 'viem';
-import { useFilters } from '../../../../../sdk/src/react';
+import { useFiltersProgressive } from '../../../../../sdk/src/react';
 import { PropertyFilters } from './PropertyFilters';
 
 type FiltersSidebarProps = {
@@ -10,35 +9,22 @@ type FiltersSidebarProps = {
 };
 
 function FiltersSidebar({ chainId, collectionAddress }: FiltersSidebarProps) {
-	const [excludePropertyValues, setExcludePropertyValues] = useState(true);
 	const {
 		data: collectableFilters,
 		isLoading: collectableFiltersLoading,
-		error: collectableFiltersError,
-	} = useFilters({
+		error,
+		isLoadingNames,
+		isFetchingValues,
+	} = useFiltersProgressive({
 		chainId,
 		collectionAddress,
-		excludePropertyValues: excludePropertyValues,
 	});
-
-	const filterNamesLoaded = collectableFilters?.every(
-		(filter) => filter.values?.length === 0,
-	);
-	const filterValuesLoaded = !collectableFilters?.every(
-		(filter) => filter.values?.length === 0,
-	);
-
-	useEffect(() => {
-		if (filterNamesLoaded) {
-			setExcludePropertyValues(false);
-		}
-	}, [filterNamesLoaded]);
 
 	return (
 		<div className="[&>div]:before:to-transparent">
 			<Scroll className={'h-full pr-0'}>
 				<div className={'flex w-full flex-col'}>
-					{collectableFiltersError ? (
+					{error ? (
 						<div className="flex flex-col rounded-md bg-background-error">
 							<Text className="text-error text-xs">
 								Failed to load filters. Please try again.
@@ -50,10 +36,8 @@ function FiltersSidebar({ chainId, collectionAddress }: FiltersSidebarProps) {
 						<div className="flex flex-col gap-3" style={{ width: 200 }}>
 							<PropertyFilters
 								filters={collectableFilters}
-								filterNamesLoading={collectableFiltersLoading}
-								filterValuesLoading={
-									!collectableFiltersLoading && !filterValuesLoaded
-								}
+								filterNamesLoading={isLoadingNames}
+								filterValuesLoading={isFetchingValues}
 							/>
 						</div>
 					) : null}
