61ad7970bda117d12bba0dc0dbcfae9ddc11fda5
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-04-29 18:53:26 +0200

    new sdk config

--- sdk/src/types/sdk-config.ts
+++ sdk/src/types/sdk-config.ts
@@ -1,18 +1,13 @@
+import type { MarketplaceConfig } from './builder-types';
+
 export type Env = 'development' | 'next' | 'production';
 
 export type SdkConfig = {
 	projectAccessKey: string;
 	projectId: string;
-	wallet?: {
-		walletConnectProjectId?: string;
-		embedded?: {
-			waasConfigKey: string;
-			googleClientId?: string;
-			appleClientId?: string;
-			appleRedirectURI?: string;
-		};
-	};
+	walletConnectProjectId?: string;
 	_internal?: {
+		prefetchedMarketplaceConfig?: MarketplaceConfig;
 		devAccessKey?: string;
 		nextAccessKey?: string;
 		builderEnv?: Env;
