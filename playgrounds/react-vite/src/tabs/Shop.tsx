69e7600643a80ec0eba3863765d4d3a8ca4ffb4a
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-05-08 09:47:23 +0200

    clen shop

--- playgrounds/react-vite/src/tabs/Shop.tsx
+++ playgrounds/react-vite/src/tabs/Shop.tsx
@@ -1,12 +1,17 @@
 import { Text } from '@0xsequence/design-system';
-import { ShopCollectibleCard } from '@0xsequence/marketplace-sdk/react';
-import { useTokenMetadata } from '@0xsequence/marketplace-sdk/react';
+import {
+	ShopCollectibleCard,
+	useTokenMetadata,
+} from '@0xsequence/marketplace-sdk/react';
+import { useReadContract } from 'wagmi';
+import { ERC1155_SALES_CONTRACT_ABI } from '../../../../sdk/src';
 
 export function Shop() {
 	const tokenIds = [1, 2, 3];
 	const chainId = 80002;
 	const contractAddress = '0x98d2dd98e762492435c731346c799145d4e61e5b';
 	const salesContractAddress = '0xddc7029ce8390cdd6b6c1ff58d4bf4c3f1f88bed';
+	const supply = 100;
 
 	const { data: tokenMetadata, isLoading: tokenMetadataLoading } =
 		useTokenMetadata({
@@ -15,6 +20,13 @@ export function Shop() {
 			tokenIds,
 		});
 
+	// const { data: supplyData } = useReadContract({
+	//   address: salesContractAddress,
+	//   abi: ERC1155_SALES_CONTRACT_ABI,
+	//   functionName: "getSupply",
+	//   args: tokenIds,
+	// });
+
 	return (
 		<div className="flex flex-col gap-4 pt-3">
 			<div className="flex items-center justify-between">
@@ -22,21 +34,25 @@ export function Shop() {
 			</div>
 
 			<div className="grid grid-cols-1 items-center justify-center gap-4 md:grid-cols-3 lg:grid-cols-4">
-				{tokenIds.map((tokenId) => (
-					<ShopCollectibleCard
-						chainId={chainId}
-						key={tokenId}
-						collectionAddress={contractAddress}
-						collectibleId={tokenId.toString()}
-						//@ts-ignore this should probably accept undefined
-						tokenMetadata={tokenMetadata?.find(
-							(token) => token.tokenId === tokenId.toString(),
-						)}
-						cardLoading={tokenMetadataLoading}
-						supply={0}
-						salesContractAddress={salesContractAddress}
-					/>
-				))}
+				{tokenIds.map((tokenId) => {
+					const token = tokenMetadata?.find(
+						(token) => token.tokenId === tokenId.toString(),
+					);
+
+					return (
+						<ShopCollectibleCard
+							chainId={chainId}
+							key={tokenId}
+							collectionAddress={contractAddress}
+							collectibleId={tokenId.toString()}
+							//@ts-ignore this should probably accept undefined
+							tokenMetadata={token}
+							cardLoading={tokenMetadataLoading}
+							supply={supply}
+							salesContractAddress={salesContractAddress}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
