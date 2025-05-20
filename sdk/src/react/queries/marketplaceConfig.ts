62161eb1b75f20273eb2f1bfcaab68ff1fa8ddda
Mithat Akbulut <marmarisco860@gmail.com>
2025-05-20 12:16:09 +0300

    temp - use placeholder shop collection

--- sdk/src/react/queries/marketplaceConfig.ts
+++ sdk/src/react/queries/marketplaceConfig.ts
@@ -170,7 +170,20 @@ export const marketplaceConfigOptions = (config: SdkConfig) => {
 				projectId,
 				projectAccessKey,
 				prefetchedMarketplaceSettings,
-				tmpShopConfig: config.tmpShopConfig,
+				tmpShopConfig: {
+					title: '',
+					bannerUrl: '',
+					collections: [
+						{
+							address: '0xf2ea13ce762226468deac9d69c8e77d291821676',
+							chainId: 80002,
+							tokenIds: ['1', '2', '3'],
+							bannerUrl: '',
+							primarySalesContractAddress:
+								'0x30131575129ee043f9c7409ca599bfd8ffe1b4e0',
+						},
+					],
+				},
 			}),
 	});
 };
