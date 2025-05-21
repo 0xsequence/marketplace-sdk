07fd1149db41ee7e728cf8c8a759a9da1485d3dc
Mithat Akbulut <marmarisco860@gmail.com>
2025-05-21 11:16:49 +0300

    Add ERC721SaleControls component and integrate it into Collectibles tab for enhanced sale functionality
    - Introduced ERC721SaleControls component to manage sale actions for ERC721 collectibles.
    - Updated Collectibles component to conditionally render ERC721SaleControls based on marketplace type and collection type.
    - Improved handling of sale configuration and collection data retrieval using hooks from the SDK.

---
+++ playgrounds/react-vite/src/tabs/components/ERC721SaleControls.tsx
@@ -0,0 +1,78 @@
+import { Button, CartIcon, Text } from '@0xsequence/design-system';
+import { useState } from 'react';
+import { type Address, formatUnits } from 'viem';
+import {
+	useCurrency,
+	useList721ShopCardData,
+} from '../../../../../sdk/src/react';
+
+type ERC721SaleControlsProps = {
+	salesContractAddress: Address;
+	collectionAddress: Address;
+	chainId: number;
+};
+
+export default function ERC721SaleControls({
+	salesContractAddress,
+	collectionAddress,
+	chainId,
+}: ERC721SaleControlsProps) {
+	const [quantity, setQuantity] = useState(1);
+	const { salePrice } = useList721ShopCardData({
+		contractAddress: collectionAddress,
+		chainId,
+		tokenIds: ['1'],
+		salesContractAddress,
+	});
+
+	const { data: currency } = useCurrency({
+		currencyAddress: salePrice?.currencyAddress,
+		chainId,
+	});
+
+	return (
+		<div className="flex w-full items-center gap-4 rounded-sm bg-background-raised p-4">
+			<div className="flex items-center">
+				<img
+					src={currency?.imageUrl}
+					alt={currency?.symbol}
+					className="mr-2 h-4 w-4"
+				/>
+
+				<Text fontWeight="medium" className="mr-1 text-text-100">
+					{formatUnits(BigInt(salePrice.amount), currency?.decimals ?? 0)}
+				</Text>
+				<Text fontWeight="bold" className="text-text-100">
+					{currency?.symbol}
+				</Text>
+			</div>
+
+			<input
+				type="number"
+				min="1"
+				value={quantity}
+				onChange={(e) =>
+					setQuantity(Math.max(1, Number.parseInt(e.target.value) || 1))
+				}
+				placeholder="Enter quantity"
+				className="w-24 rounded-md border border-border-base bg-background-default px-4 py-2 text-text-100 placeholder-text-40 focus:border-border-focus focus:outline-none focus:ring-1 focus:ring-border-focus"
+			/>
+
+			<Button
+				variant="primary"
+				label="Buy"
+				leftIcon={CartIcon}
+				onClick={() => console.log(`Buying ${quantity} items`)}
+			/>
+
+			<Text variant="small" className="text-text-50">
+				(Total:{' '}
+				{formatUnits(
+					BigInt(salePrice.amount) * BigInt(quantity),
+					currency?.decimals ?? 0,
+				)}{' '}
+				{currency?.symbol})
+			</Text>
+		</div>
+	);
+}
