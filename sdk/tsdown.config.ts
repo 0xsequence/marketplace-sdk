cb0ee2e7cf13024feda28519618d2ba920eb295e
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-05-31 10:54:21 +0200

    change from postcss to tailwind-cli

--- sdk/tsdown.config.ts
+++ sdk/tsdown.config.ts
@@ -1,15 +1,23 @@
-// import preserveDirectives from 'rollup-preserve-directives';
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
+import preserveDirectives from 'rollup-preserve-directives';
 import { defineConfig } from 'tsdown';
+
+const execAsync = promisify(exec);
 
 export default defineConfig([
 	{
 		entry: ['src/**/index.ts', '!src/**/__tests__/**', '!src/**/*.test.*'],
 		dts: true,
+		platform: 'neutral',
 		sourcemap: true,
 		format: ['esm'],
 		outDir: 'dist',
 		clean: true,
-		//  plugins: [preserveDirectives()],
+		plugins: [preserveDirectives()],
+		onSuccess: () => {
+			execAsync('pnpm tailwindcss -i ./src/index.css -o ./dist/index.css');
+		},
 		loader: {
 			'.png': 'dataurl',
 		},
