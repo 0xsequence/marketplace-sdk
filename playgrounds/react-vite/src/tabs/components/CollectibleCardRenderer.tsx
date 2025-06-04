bad2247753c744611e5d0ad67113d2395699f0f0
Mithat Akbulut <marmarisco860@gmail.com>
2025-06-04 17:26:31 +0300

    Add hooks and components for inventory and sale controls

---
+++ playgrounds/react-vite/src/tabs/components/CollectibleCardRenderer.tsx
@@ -0,0 +1,53 @@
+import { Text } from '@0xsequence/design-system';
+import { CollectibleCard } from '@0xsequence/marketplace-sdk/react';
+import { Link } from 'react-router';
+import type { CollectibleCardProps } from '../../../../../sdk/src/react/ui/components/marketplace-collectible-card';
+
+interface CollectibleCardRendererProps {
+	index: number;
+	collectibleCard: CollectibleCardProps;
+	isLoading: boolean;
+}
+
+export function CollectibleCardRenderer({
+	index,
+	collectibleCard,
+	isLoading,
+}: CollectibleCardRendererProps) {
+	if (isLoading) {
+		return (
+			<div className="flex w-full min-w-[175px] items-stretch justify-center">
+				<Text>Loading...</Text>
+			</div>
+		);
+	}
+
+	if (collectibleCard.marketplaceType === 'shop') {
+		const now = Date.now() / 1000; // Convert to seconds to match the Unix timestamp
+		const saleStartsAt = collectibleCard.saleStartsAt
+			? Number(collectibleCard.saleStartsAt)
+			: null;
+		const saleEndsAt = collectibleCard.saleEndsAt
+			? Number(collectibleCard.saleEndsAt)
+			: null;
+
+		const isSaleActive =
+			saleStartsAt && saleEndsAt && saleStartsAt <= now && saleEndsAt >= now;
+
+		// If sale is not active, don't render the card
+		if (!isSaleActive) {
+			return null;
+		}
+	}
+
+	return (
+		<div
+			key={index}
+			className="flex w-full min-w-[175px] items-stretch justify-center"
+		>
+			<Link to={'/collectible'} key={collectibleCard.collectibleId}>
+				<CollectibleCard {...collectibleCard} />
+			</Link>
+		</div>
+	);
+}
