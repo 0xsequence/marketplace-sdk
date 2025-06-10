c1b2cf7e162d9674e36b032e78b2a573f6ae8ba9
Mithat Akbulut <marmarisco860@gmail.com>
2025-06-10 15:02:22 +0300

    use exact collectibles from marketplace config

--- playgrounds/react-vite/src/tabs/Collectables.tsx
+++ playgrounds/react-vite/src/tabs/Collectables.tsx
@@ -2,7 +2,11 @@ import { Text } from '@0xsequence/design-system';
 import { FilterBadges, useMarketplace } from 'shared-components';
 import type { Address } from 'viem';
 import { ContractType } from '../../../../sdk/src';
-import { useCollection, useMarketplaceConfig } from '../../../../sdk/src/react';
+import {
+	useCollection,
+	useListPrimarySaleItems,
+	useMarketplaceConfig,
+} from '../../../../sdk/src/react';
 import ERC721SaleControls from './components/ERC721SaleControls';
 import { MarketContent } from './components/MarketContent';
 import { ShopContent } from './components/ShopContent';
@@ -16,9 +20,20 @@ export function Collectibles() {
 	);
 	const isShop = marketplaceType === 'shop';
 	const saleContractAddress = saleConfig?.saleAddress as Address;
-	// Edit this to get the item ids
-	const saleItemIds = ['0', '1'];
-	const { data: collection } = useCollection({
+
+	const { data: primarySaleItems, isLoading: isLoadingPrimarySaleItems } =
+		useListPrimarySaleItems({
+			chainId,
+			primarySaleContractAddress: saleContractAddress,
+			enabled: isShop,
+		});
+
+	const saleItemIds =
+		primarySaleItems?.primarySaleItems
+			.map((item) => item.primarySaleItem.tokenId?.toString() ?? '')
+			.filter(Boolean) ?? [];
+
+	const { data: collection, isLoading: isLoadingCollection } = useCollection({
 		collectionAddress,
 		chainId,
 		query: {
@@ -43,7 +58,8 @@ export function Collectibles() {
 					chainId={chainId}
 					salesContractAddress={saleContractAddress}
 					collectionAddress={collectionAddress}
-					tokenIds={saleItemIds || []}
+					tokenIds={saleItemIds}
+					isLoading={isLoadingCollection || isLoadingPrimarySaleItems}
 				/>
 			)}
 
