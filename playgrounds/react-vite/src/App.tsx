822ff0fbdd47092ee1640ecb3405cef9ab4a1d3b
Mithat Akbulut <marmarisco860@gmail.com>
2025-06-10 14:54:02 +0300

    use overridessettings component

--- playgrounds/react-vite/src/App.tsx
+++ playgrounds/react-vite/src/App.tsx
@@ -1,6 +1,6 @@
 import { Button, Divider, Text } from '@0xsequence/design-system';
 import { Outlet, useLocation, useNavigate } from 'react-router';
-import { Navigation, Settings } from 'shared-components';
+import { Navigation, OverridesSettings } from 'shared-components';
 import { useMarketplace } from 'shared-components';
 import { ROUTES } from './lib/routes';
 
@@ -13,23 +13,17 @@ function App() {
 		<div style={{ width: '100vw', paddingBlock: '70px' }}>
 			<div className="m-auto flex w-[95%] max-w-[800px] flex-col gap-3">
 				<Text variant="xlarge">Sequence Marketplace SDK Playground</Text>
-				<Settings />
+				<OverridesSettings />
 				<div className="mb-2 flex flex-row gap-3 rounded-xl bg-background-raised p-3">
 					<Button
 						variant={marketplaceType === 'market' ? 'primary' : 'base'}
-						onClick={() => {
-							setMarketplaceType('market');
-							navigate(`/${ROUTES.COLLECTIONS.path}`);
-						}}
+						onClick={() => setMarketplaceType('market')}
 					>
 						Market
 					</Button>
 					<Button
 						variant={marketplaceType === 'shop' ? 'primary' : 'base'}
-						onClick={() => {
-							setMarketplaceType('shop');
-							navigate(`/${ROUTES.COLLECTIONS.path}`);
-						}}
+						onClick={() => setMarketplaceType('shop')}
 					>
 						Shop
 					</Button>
