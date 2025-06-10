d89a004438a8248b416adeb5abf88801a8e7ac49
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-06-10 12:19:29 +0200

    Improve use list primary sales items

--- sdk/src/react/hooks/useListPrimarySaleItems.ts
+++ sdk/src/react/hooks/useListPrimarySaleItems.ts
@@ -1,42 +1,59 @@
-import { queryOptions, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import type { SdkConfig } from '../../types';
 import {
 	type ListPrimarySaleItemsArgs,
+	type ListPrimarySaleItemsReturn,
+	type Page,
+	type PrimarySaleItemsFilter,
 	getMarketplaceClient,
 } from '../_internal';
 import { useConfig } from './useConfig';
 
-type UseListPrimarySaleItemsArgs = Omit<ListPrimarySaleItemsArgs, 'chainId'> & {
+type UseListPrimarySaleItemsArgs = {
 	chainId: number;
+	primarySaleContractAddress: string;
+	filter?: PrimarySaleItemsFilter;
+	query?: {
+		enabled?: boolean;
+	};
 };
 
 const fetchListPrimarySaleItems = async (
 	config: SdkConfig,
-	args: UseListPrimarySaleItemsArgs,
+	args: ListPrimarySaleItemsArgs,
 ) => {
-	const arg = {
-		chainId: String(args.chainId),
-		primarySaleContractAddress: args.primarySaleContractAddress,
-		filter: args.filter,
-		page: args.page,
-	} satisfies ListPrimarySaleItemsArgs;
-
 	const marketplaceClient = getMarketplaceClient(config);
-	return marketplaceClient.listPrimarySaleItems(arg);
+	return marketplaceClient.listPrimarySaleItems(args);
 };
 
 export const listPrimarySaleItemsOptions = (
 	args: UseListPrimarySaleItemsArgs,
 	config: SdkConfig,
 ) => {
-	return queryOptions({
+	return {
 		queryKey: ['primarySaleItems', args, config],
-		queryFn: () => fetchListPrimarySaleItems(config, args),
-	});
+		queryFn: async ({ pageParam }: { pageParam: Page }) => {
+			return fetchListPrimarySaleItems(config, {
+				chainId: String(args.chainId),
+				primarySaleContractAddress: args.primarySaleContractAddress,
+				filter: args.filter,
+				page: pageParam,
+			});
+		},
+		initialPageParam: { page: 1, pageSize: 30 } as Page,
+		getNextPageParam: (lastPage: ListPrimarySaleItemsReturn) =>
+			lastPage.page?.more
+				? {
+						page: (lastPage.page?.page || 1) + 1,
+						pageSize: lastPage.page?.pageSize || 30,
+					}
+				: undefined,
+		enabled: args.query?.enabled ?? true,
+	};
 };
 
 export const useListPrimarySaleItems = (args: UseListPrimarySaleItemsArgs) => {
 	const config = useConfig();
 
-	return useQuery(listPrimarySaleItemsOptions(args, config));
+	return useInfiniteQuery(listPrimarySaleItemsOptions(args, config));
 };
