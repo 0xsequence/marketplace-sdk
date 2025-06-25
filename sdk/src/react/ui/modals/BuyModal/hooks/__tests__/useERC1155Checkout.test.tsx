eade958127ab5c2da91a6a934c983a49575affd9
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-06-25 09:53:36 +0200

    Update snapshot

--- sdk/src/react/ui/modals/BuyModal/hooks/__tests__/useERC1155Checkout.test.tsx
+++ sdk/src/react/ui/modals/BuyModal/hooks/__tests__/useERC1155Checkout.test.tsx
@@ -87,36 +87,37 @@ describe('useERC1155Checkout', () => {
 		expect(
 			mockUseERC1155SaleContractCheckout.mock.calls,
 		).toMatchInlineSnapshot(`
-      [
-        [
-          {
-            "chain": 1,
-            "checkoutOptions": {
-              "crypto": "all",
-              "nftCheckout": [],
-              "onRamp": [],
-              "swap": [],
-            },
-            "collectionAddress": "0x123",
-            "contractAddress": "0x456",
-            "customProviderCallback": undefined,
-            "items": [
-              {
-                "quantity": "2",
-                "tokenId": "1",
-              },
-            ],
-            "onClose": [Function],
-            "onError": [Function],
-            "onSuccess": [Function],
-            "supplementaryAnalyticsInfo": {
-              "type": "mint_shop",
-            },
-            "wallet": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-          },
-        ],
-      ]
-    `);
+			[
+			  [
+			    {
+			      "chain": 1,
+			      "chainId": "1",
+			      "checkoutOptions": {
+			        "crypto": "all",
+			        "nftCheckout": [],
+			        "onRamp": [],
+			        "swap": [],
+			      },
+			      "collectionAddress": "0x123",
+			      "contractAddress": "0x456",
+			      "customProviderCallback": undefined,
+			      "items": [
+			        {
+			          "quantity": "2",
+			          "tokenId": "1",
+			        },
+			      ],
+			      "onClose": [Function],
+			      "onError": [Function],
+			      "onSuccess": [Function],
+			      "supplementaryAnalyticsInfo": {
+			        "type": "mint_shop",
+			      },
+			      "wallet": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+			    },
+			  ],
+			]
+		`);
 
 		expect(result.current.isEnabled).toBe(true);
 	});
