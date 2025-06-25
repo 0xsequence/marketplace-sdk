453cfd415ac794068102bf9411bf9a67399fbb82
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-06-25 10:59:51 +0200

    Integrate SmartWalletConnectionProvider into MarketplaceProvider

--- sdk/src/react/provider.tsx
+++ sdk/src/react/provider.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { createContext } from 'react';
 import type { SdkConfig } from '../types';
 import { InvalidProjectAccessKeyError } from '../utils/_internal/error/config';
+import { SmartWalletConnectionProvider } from './_internal/wallet';
 import { getQueryClient } from './_internal/api/get-query-client';
 import { PROVIDER_ID } from './_internal/get-provider';
 
@@ -36,7 +37,9 @@ export function MarketplaceProvider({
 	return (
 		<MarketplaceQueryClientProvider>
 			<MarketplaceSdkContext.Provider value={config}>
-				<div id={PROVIDER_ID}>{children}</div>
+				<SmartWalletConnectionProvider>
+					<div id={PROVIDER_ID}>{children}</div>
+				</SmartWalletConnectionProvider>
 			</MarketplaceSdkContext.Provider>
 		</MarketplaceQueryClientProvider>
 	);
