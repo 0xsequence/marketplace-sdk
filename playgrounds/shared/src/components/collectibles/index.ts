574666579074834dcb7b26bea723a0c1aa3b200d
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-06-25 16:38:02 +0200

    Remove unused GridContainer and update component exports

--- playgrounds/shared/src/components/collectibles/index.ts
+++ playgrounds/shared/src/components/collectibles/index.ts
@@ -1,7 +1,8 @@
 export { CollectibleCardRenderer } from './CollectibleCardRenderer';
 export { CollectibleGrid } from './CollectibleGrid';
-export { GridContainer } from './GridContainer';
 export type { InfiniteScrollViewProps } from './InfiniteScrollView';
 export { InfiniteScrollView } from './InfiniteScrollView';
 export type { PaginatedViewProps } from './PaginatedView';
 export { PaginatedView } from './PaginatedView';
+export type { VirtualizedCollectiblesViewProps } from './VirtualizedCollectiblesView';
+export { VirtualizedCollectiblesView } from './VirtualizedCollectiblesView';
