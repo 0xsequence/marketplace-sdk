a7f49dfd41dfc64f658cd7e1511077f15fa91947
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-06-25 10:59:26 +0200

    Add wallet connection abstraction interface and context

---
+++ sdk/src/react/_internal/wallet/index.ts
@@ -0,0 +1,4 @@
+export { WalletConnectionContext, useWalletConnection } from './WalletConnectionContext';
+export { SequenceWalletConnectionProvider } from './providers/SequenceWalletConnectionProvider';
+export { SmartWalletConnectionProvider } from './providers/SmartWalletConnectionProvider';
+export type { WalletConnectionHandler } from './types';
