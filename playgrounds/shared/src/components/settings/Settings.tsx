1f40f20d227d5dc6db9f2374ff11818dd5b8615b
Mithat Akbulut <marmarisco860@gmail.com>
2025-06-26 16:05:40 +0300

    refactor UI

--- playgrounds/shared/src/components/settings/Settings.tsx
+++ playgrounds/shared/src/components/settings/Settings.tsx
@@ -102,7 +102,7 @@ export function Settings() {
 	return (
 		<Collapsible
 			className="!bg-background-raised mb-2"
-			defaultOpen={true}
+			defaultOpen={false}
 			label={
 				<div className="flex items-center gap-2">
 					<Text>Configuration & Overrides</Text>
