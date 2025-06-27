8779783c15f1bd7e2a4f8e6c52e67ee7ee49a902
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-06-27 08:19:58 +0200

    Add Tailwind and PostCSS configs for Storybook

---
+++ sdk/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+		'./stories/**/*.{js,ts,jsx,tsx,mdx}',
+		'./.storybook/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
