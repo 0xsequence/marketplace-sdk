7b6516969537a55139f1985c0ec96a7c69c39471
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-07-04 10:33:59 +0200

    Add classname overwrite to skeleton

--- sdk/src/react/ui/components/media/MediaSkeleton.tsx
+++ sdk/src/react/ui/components/media/MediaSkeleton.tsx
@@ -1,11 +1,15 @@
 import { Skeleton } from '@0xsequence/design-system';
+import { cn } from '../../../../utils';
 
-export default function MediaSkeleton() {
+export default function MediaSkeleton({ className }: { className?: string }) {
 	return (
 		<Skeleton
 			data-testid="media"
 			size="lg"
-			className="absolute inset-0 h-full w-full animate-shimmer"
+			className={cn(
+				'absolute inset-0 h-full w-full animate-shimmer',
+				className,
+			)}
 			style={{
 				borderRadius: 0,
 			}}
