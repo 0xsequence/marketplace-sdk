31c9e8cbe58f9c734e6ae5efb9a629c9cb0167be
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-07-11 15:24:52 +0200

    Move shop hooks

--- sdk/src/react/hooks/data/marketplace/index.ts
+++ sdk/src/react/hooks/data/marketplace/index.ts
@@ -1,5 +1,1 @@
-export * from './useList721ShopCardData';
-export * from './useList1155ShopCardData';
 export * from './useListMarketCardData';
-export * from './useListShopCardData';
-export * from './useShopCollectibleSaleData';
