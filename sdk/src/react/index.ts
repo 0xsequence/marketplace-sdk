fa4eed17d80a64678c08aa1d184cbd4020bd45d5
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-07-13 12:32:41 +0200

    remove options folder

--- sdk/src/react/index.ts
+++ sdk/src/react/index.ts
@@ -1,7 +1,6 @@
 export * from './_internal/api/get-query-client';
 export * from './_internal/wagmi/create-config';
 export * from './hooks';
-export * from './hooks/options';
 export * from './provider';
 export * from './queries';
 export * from './types/query';
