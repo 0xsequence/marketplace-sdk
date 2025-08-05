320dbd0a601a494346fa4cfd9e4c7524a7724508
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-08-05 12:11:37 +0300

    remove depricated hook

--- sdk/src/react/hooks/data/primary-sales/index.ts
+++ sdk/src/react/hooks/data/primary-sales/index.ts
@@ -1,7 +1,6 @@
 export * from './useCountOfPrimarySaleItems';
 export * from './useGetCountOfPrimarySaleItems';
 export * from './useList721ShopCardData';
-export * from './useList1155SaleSupplies';
 export * from './useList1155ShopCardData';
 export * from './useListPrimarySaleItems';
 export * from './useShopCollectibleSaleData';
