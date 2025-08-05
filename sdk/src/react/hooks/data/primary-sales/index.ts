cbb936a108cb82b10327142a7eacdb2b6eb0e51a
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-08-05 13:42:39 +0300

    remove old hook

--- sdk/src/react/hooks/data/primary-sales/index.ts
+++ sdk/src/react/hooks/data/primary-sales/index.ts
@@ -5,4 +5,3 @@ export * from './useList721ShopCardData';
 export * from './useList1155ShopCardData';
 export * from './useListPrimarySaleItems';
 export * from './useShopCollectibleSaleData';
-export * from './useTokenSaleDetailsBatch';
