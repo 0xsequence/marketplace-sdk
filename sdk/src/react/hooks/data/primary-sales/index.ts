7fdfd79f8afe3efb8bf48eece7f65f3c41da59fd
Mithat Akbulut <marmarisco860@gmail.com>
2025-08-06 13:02:09 +0300

    remove `useShopCollectibleSaleData`

--- sdk/src/react/hooks/data/primary-sales/index.ts
+++ sdk/src/react/hooks/data/primary-sales/index.ts
@@ -4,4 +4,3 @@ export * from './useGetCountOfPrimarySaleItems';
 export * from './useList721ShopCardData';
 export * from './useList1155ShopCardData';
 export * from './useListPrimarySaleItems';
-export * from './useShopCollectibleSaleData';
