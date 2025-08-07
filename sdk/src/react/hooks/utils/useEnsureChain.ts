b23d6cfbb7652e0edda137ecce06c3adb2e4c2fe
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-08-07 10:18:28 +0300

    UseEnsureChain hook

---
+++ sdk/src/react/hooks/utils/useEnsureChain.ts
@@ -0,0 +1,74 @@
+import { useCallback } from 'react';
+import { useAccount, useSwitchChain } from 'wagmi';
+import { ChainSwitchUserRejectedError } from '../../../utils/_internal/error/transaction';
+import { useSwitchChainModal } from '../../ui/modals/_internal/components/switchChainModal';
+import { useConnectorMetadata } from '../config/useConnectorMetadata';
+
+export const useEnsureChain = () => {
+	const { chainId: currentChainId } = useAccount();
+	const { switchChain, switchChainAsync } = useSwitchChain();
+	const { show: showSwitchChainModal } = useSwitchChainModal();
+	const { isWaaS } = useConnectorMetadata();
+
+	const ensureChainAsync = useCallback(
+		async (targetChainId: number) => {
+			if (currentChainId === targetChainId) {
+				return Promise.resolve();
+			}
+			if (isWaaS) {
+				return switchChainAsync({ chainId: targetChainId });
+			}
+
+			return new Promise((resolve, reject) => {
+				showSwitchChainModal({
+					chainIdToSwitchTo: targetChainId,
+					onSuccess: () => resolve(targetChainId),
+					onError: (error) => reject(error),
+					onClose: () => reject(new ChainSwitchUserRejectedError()),
+				});
+			});
+		},
+		[currentChainId, isWaaS, switchChainAsync, showSwitchChainModal],
+	);
+
+	const ensureChain = useCallback(
+		(
+			targetChainId: number,
+			callbacks?: {
+				onSuccess?: () => void;
+				onError?: (error: Error) => void;
+				onClose?: () => void;
+			},
+		) => {
+			if (currentChainId === targetChainId) {
+				callbacks?.onSuccess?.();
+				return;
+			}
+
+			if (isWaaS) {
+				switchChain(
+					{ chainId: targetChainId },
+					{
+						onSuccess: callbacks?.onSuccess,
+						onError: callbacks?.onError,
+					},
+				);
+				return;
+			}
+
+			showSwitchChainModal({
+				chainIdToSwitchTo: targetChainId,
+				onSuccess: callbacks?.onSuccess,
+				onError: callbacks?.onError,
+				onClose: callbacks?.onClose,
+			});
+		},
+		[currentChainId, isWaaS, switchChain, showSwitchChainModal],
+	);
+
+	return {
+		ensureChain,
+		ensureChainAsync,
+		currentChainId,
+	};
+};
