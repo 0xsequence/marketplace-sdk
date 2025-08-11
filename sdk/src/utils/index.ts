c2dc19b68eecc3a2be2a56e2592467c78f1b7006
Mithat Akbulut <marmarisco860@gmail.com>
2025-08-11 13:31:44 +0300

    remove `extractUserAddressFromCookies`

--- sdk/src/utils/index.ts
+++ sdk/src/utils/index.ts
@@ -1,7 +1,6 @@
 export * from './abi';
 export * from './address';
 export * from './cn';
-export * from './extractUserAddressFromCookies';
 export * from './getMarketplaceDetails';
 export * from './network';
 export * from './networkconfigToWagmiChain';
