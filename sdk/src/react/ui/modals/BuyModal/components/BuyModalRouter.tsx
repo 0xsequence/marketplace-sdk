4b65bd8c9018d3ddebd3cd4b0a3f8952bf83a5ad
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-08-14 17:16:21 +0300

    Allways use 721 buy flow

--- sdk/src/react/ui/modals/BuyModal/components/BuyModalRouter.tsx
+++ sdk/src/react/ui/modals/BuyModal/components/BuyModalRouter.tsx
@@ -11,8 +11,8 @@ import {
 } from '../store';
 import { ERC721BuyModal } from './ERC721BuyModal';
 import { ERC721ShopModal } from './ERC721ShopModal';
-import { ERC1155BuyModal } from './ERC1155BuyModal';
-import { ERC1155ShopModal } from './ERC1155ShopModal';
+// import { ERC1155BuyModal } from './ERC1155BuyModal';
+// import { ERC1155ShopModal } from './ERC1155ShopModal';
 
 export const BuyModalRouter = () => {
 	const modalProps = useBuyModalProps();
@@ -54,104 +54,80 @@ export const BuyModalRouter = () => {
 	}
 
 	if (isShop) {
-		if (collection.type === 'ERC721') {
-			if (!shopData || !currency) {
-				return (
-					<LoadingModal
-						isOpen={true}
-						chainId={chainId}
-						onClose={() => buyModalStore.send({ type: 'close' })}
-						title="Loading payment options"
-					/>
-				);
-			}
+		// if (collection.type === 'ERC721') {
+		if (!shopData || !currency) {
 			return (
-				<ERC721ShopModal
-					collection={collection}
-					shopData={shopData}
+				<LoadingModal
+					isOpen={true}
 					chainId={chainId}
+					onClose={() => buyModalStore.send({ type: 'close' })}
+					title="Loading payment options"
 				/>
 			);
 		}
-		if (collection.type === 'ERC1155') {
-			if (!shopData || !currency) {
-				return (
-					<LoadingModal
-						isOpen={true}
-						chainId={chainId}
-						onClose={() => buyModalStore.send({ type: 'close' })}
-						title="Loading payment options"
-					/>
-				);
-			}
-			return (
-				<ERC1155ShopModal
-					collection={collection}
-					shopData={shopData}
-					chainId={chainId}
-				/>
-			);
-		}
-	} else {
-		if (collection.type === 'ERC721') {
-			if (!collectable || !order || !address || !checkoutOptions) {
-				return (
-					<LoadingModal
-						isOpen={true}
-						chainId={chainId}
-						onClose={() => buyModalStore.send({ type: 'close' })}
-						title="Loading payment options"
-					/>
-				);
-			}
-			return (
-				<ERC721BuyModal
-					collection={collection}
-					collectable={collectable}
-					order={order}
-					address={address}
-					checkoutOptions={checkoutOptions}
-					chainId={chainId}
-				/>
-			);
-		}
-		if (collection.type === 'ERC1155') {
-			if (!collectable || !order || !address || !checkoutOptions) {
-				return (
-					<LoadingModal
-						isOpen={true}
-						chainId={chainId}
-						onClose={() => buyModalStore.send({ type: 'close' })}
-						title="Loading payment options"
-					/>
-				);
-			}
-			return (
-				<ERC1155BuyModal
-					collection={collection}
-					collectable={collectable}
-					order={order}
-					address={address}
-					checkoutOptions={checkoutOptions}
-					chainId={chainId}
-				/>
-			);
-		}
+		return (
+			<ERC721ShopModal
+				collection={collection}
+				shopData={shopData}
+				chainId={chainId}
+			/>
+		);
+		// }
+		// if (collection.type === 'ERC1155') {
+		// 	if (!shopData || !currency) {
+		// 		return (
+		// 			<LoadingModal
+		// 				isOpen={true}
+		// 				chainId={chainId}
+		// 				onClose={() => buyModalStore.send({ type: 'close' })}
+		// 				title="Loading payment options"
+		// 			/>
+		// 		);
+		// 	}
+		// 	return (
+		// 		<ERC1155ShopModal
+		// 			collection={collection}
+		// 			shopData={shopData}
+		// 			chainId={chainId}
+		// 		/>
+		// 	);
+		// }
 	}
-
-	onError(
-		new Error(
-			`Unsupported configuration: ${collection.type} in ${
-				isShop ? 'shop' : 'market'
-			} mode`,
-		),
-	);
+	// if (collection.type === 'ERC721') {
+	if (!collectable || !order || !address || !checkoutOptions) {
+		return (
+			<LoadingModal
+				isOpen={true}
+				chainId={chainId}
+				onClose={() => buyModalStore.send({ type: 'close' })}
+				title="Loading payment options"
+			/>
+		);
+	}
 	return (
-		<ErrorModal
-			isOpen={true}
+		<ERC721BuyModal
+			collection={collection}
+			collectable={collectable}
+			order={order}
+			address={address}
+			checkoutOptions={checkoutOptions}
 			chainId={chainId}
-			onClose={() => buyModalStore.send({ type: 'close' })}
-			title="Unsupported Configuration"
 		/>
 	);
+
+	// onError(
+	// 	new Error(
+	// 		`Unsupported configuration: ${collection.type} in ${
+	// 			isShop ? 'shop' : 'market'
+	// 		} mode`,
+	// 	),
+	// );
+	// return (
+	// 	<ErrorModal
+	// 		isOpen={true}
+	// 		chainId={chainId}
+	// 		onClose={() => buyModalStore.send({ type: 'close' })}
+	// 		title="Unsupported Configuration"
+	// 	/>
+	// );
 };
