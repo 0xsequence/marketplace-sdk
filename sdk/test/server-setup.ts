bc687db575b68fc203331eb9a14562e286f9b5ae
Mithat Akbulut <marmarisco860@gmail.com>
2025-10-16 13:41:06 +0300

    Remove LAOS references from API and update related files for consistency

--- sdk/test/server-setup.ts
+++ sdk/test/server-setup.ts
@@ -2,7 +2,6 @@ import { HttpResponse, http as mswHttp } from 'msw';
 import { setupServer } from 'msw/node';
 import { handlers as marketplaceConfigHandlers } from '../src/react/_internal/api/__mocks__/builder.msw';
 import { handlers as indexerHandlers } from '../src/react/_internal/api/__mocks__/indexer.msw';
-import { laosHandlers } from '../src/react/_internal/api/__mocks__/laos.msw';
 import { handlers as marketplaceHandlers } from '../src/react/_internal/api/__mocks__/marketplace.msw';
 import { handlers as metadataHandlers } from '../src/react/_internal/api/__mocks__/metadata.msw';
 
@@ -18,6 +17,5 @@ export const server = setupServer(
 	...metadataHandlers,
 	...indexerHandlers,
 	...marketplaceConfigHandlers,
-	...laosHandlers,
 	tickHandler,
 );
