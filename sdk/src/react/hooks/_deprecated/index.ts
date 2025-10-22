5b776a79df3669c5e6bfd3c48328ccf7f998d1af
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-10-22 12:56:54 +0300

    feat(hooks): add backward compatibility exports for renamed hooks

---
+++ sdk/src/react/hooks/_deprecated/index.ts
@@ -0,0 +1,54 @@
+export {
+	useCheckoutOptions,
+	useCheckoutOptionsSalesContract,
+} from '../checkout';
+export type { UseCollectibleMetadataParams as UseCollectibleDetailParams } from '../collectible';
+export {
+	useCollectibleBalance as useBalanceOfCollectible,
+	useCollectibleMarketActivities as useListCollectibleActivities,
+	useCollectibleMarketCount as useCountOfCollectables,
+	useCollectibleMarketHighestOffer as useHighestOffer,
+	useCollectibleMarketList as useListCollectibles,
+	useCollectibleMarketListings as useListListingsForCollectible,
+	useCollectibleMarketListingsCount as useCountListingsForCollectible,
+	useCollectibleMarketListPaginated as useListCollectiblesPaginated,
+	useCollectibleMarketLowestListing as useLowestListing,
+	useCollectibleMarketOffers as useListOffersForCollectible,
+	useCollectibleMarketOffersCount as useCountOffersForCollectible,
+	useCollectibleMetadata as useCollectible,
+	useCollectibleMetadata as useCollectibleDetail,
+	useCollectiblePrimarySaleList as useListPrimarySaleItems,
+	useCollectiblePrimarySaleListCount as useCountOfPrimarySaleItems,
+	useCollectibleTokenBalances,
+} from '../collectible';
+export type { UseCollectionMetadataParams as UseCollectionDetailParams } from '../collection';
+export {
+	useCollectionBalanceDetails,
+	useCollectionDetailPolling as useCollectionDetailsPolling,
+	useCollectionList as useListCollections,
+	useCollectionMarketActivities as useListCollectionActivities,
+	useCollectionMarketFilteredCount as useGetCountOfFilteredOrders,
+	useCollectionMarketFloor as useFloorOrder,
+	useCollectionMarketItems as useListItemsOrdersForCollection,
+	useCollectionMarketItemsCount as useCountItemsOrdersForCollection,
+	useCollectionMarketItemsPaginated as useListItemsOrdersForCollectionPaginated,
+	useCollectionMetadata as useCollection,
+	useCollectionMetadata as useCollectionDetail,
+} from '../collection';
+export type { UseCurrencyParams as UseCurrencyDetailParams } from '../currency';
+export {
+	useCurrency,
+	useCurrency as useCurrencyDetail,
+	useCurrencyComparePrices as useComparePrices,
+	useCurrencyConvertToUSD as useConvertPriceToUSD,
+	useCurrencyList as useMarketCurrencies,
+} from '../currency';
+export { useInventory } from '../inventory';
+export { useMarketplaceListCardData as useListMarketCardData } from '../marketplace';
+export {
+	useTokenBalances as useListBalances,
+	useTokenCurrencyBalance as useCurrencyBalance,
+	useTokenMetadata as useListTokenMetadata,
+	useTokenMetadataSearch as useSearchTokenMetadata,
+	useTokenRanges as useGetTokenRanges,
+} from '../token';
