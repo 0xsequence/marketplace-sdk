abdc712d72e05c997f3e90a36661b977f5bdd5e9
Mithat Akbulut <marmarisco860@gmail.com>
2025-10-27 11:31:34 +0300

    remove universal modal and its components

--- sdk/src/react/ui/modals/_internal/components/universal/index.ts
+++
@@ -1,6 +0,0 @@
-export { UniversalError, UniversalErrorExplicit } from './UniversalError';
-export {
-	UniversalLoading,
-	type UniversalLoadingProps,
-} from './UniversalLoading';
-export { UniversalModal, type UniversalModalProps } from './UniversalModal';
