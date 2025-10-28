0204262041a5ec93a405a11b428a1d9d5a054f9b
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-10-28 12:36:32 +0200

    Sell mutations

---
+++ sdk/src/react/ui/modals/SellModal/internal/sell-mutations.ts
@@ -0,0 +1,103 @@
+import { useMutation } from '@tanstack/react-query';
+import { type Address, formatUnits } from 'viem';
+import { useAccount } from 'wagmi';
+import { type Step, TransactionType } from '../../../../_internal';
+import { useAnalytics } from '../../../../_internal/databeat';
+import { useConfig, useCurrency, useProcessStep } from '../../../../hooks';
+import { waitForTransactionReceipt } from '../../../../utils';
+import { useTransactionStatusModal } from '../../_internal/components/transactionStatusModal';
+import { useSellModalState } from './store';
+import type { useGenerateSellTransaction } from './use-genrate-sell-transaction';
+
+export const useSellMutations = (
+	tx: ReturnType<typeof useGenerateSellTransaction>['data'],
+) => {
+	const sdkConfig = useConfig();
+	const { show: showTxModal } = useTransactionStatusModal();
+	const analytics = useAnalytics();
+	const state = useSellModalState();
+	const { processStep } = useProcessStep();
+	const { address } = useAccount();
+	const { data: currency } = useCurrency({
+		chainId: state.chainId,
+		currencyAddress: address,
+	});
+
+	async function executeStepAndWait(step: Step) {
+		const res = await processStep(step, state.chainId);
+		if (res.type === 'transaction' && res.hash) {
+			await waitForTransactionReceipt({
+				txHash: res.hash,
+				chainId: state.chainId,
+				sdkConfig,
+			});
+		}
+		return res;
+	}
+
+	const approve = useMutation({
+		mutationFn: async () => {
+			if (!tx?.approveStep) throw new Error('No approval step available');
+			return await executeStepAndWait(tx.approveStep);
+		},
+		onError: (e) => state.callbacks?.onError?.(e as Error),
+	});
+
+	const sell = useMutation({
+		mutationFn: async () => {
+			if (!tx?.sellStep) throw new Error('No sell step available');
+			const res = await executeStepAndWait(tx.sellStep);
+
+			if (currency && state.order?.priceAmount) {
+				const dec = currency.decimals ?? 0;
+				const raw = state.order.priceAmount;
+				const currencyValueDecimal = Number(formatUnits(BigInt(raw), dec));
+
+				analytics.trackSellItems({
+					props: {
+						marketplaceKind: state.order.marketplace,
+						userId: address || '',
+						collectionAddress: state.collectionAddress,
+						currencyAddress: currency.contractAddress as Address,
+						currencySymbol: currency.symbol || '',
+						requestId: state.order.orderId,
+						tokenId: state.tokenId,
+						chainId: String(state.chainId),
+						txnHash: res.type === 'transaction' ? res.hash : '',
+					},
+					nums: {
+						currencyValueDecimal,
+						currencyValueRaw: Number(raw),
+					},
+				});
+			}
+
+			return res;
+		},
+		onSuccess: (res) => {
+			// TODO: this should be solved in a headless way
+			state.closeModal();
+			showTxModal({
+				type: TransactionType.SELL,
+				chainId: state.chainId,
+				hash: res?.type === 'transaction' ? res.hash : undefined,
+				orderId: res?.type === 'signature' ? res.orderId : undefined,
+				callbacks: state.callbacks,
+				queriesToInvalidate: ['balance'],
+				collectionAddress: state.collectionAddress,
+				collectibleId: state.tokenId,
+			});
+
+			state.callbacks?.onSuccess?.({
+				hash: res?.type === 'transaction' ? res.hash : undefined,
+				orderId: res?.type === 'signature' ? res.orderId : undefined,
+			});
+		},
+		onError: (e) => state.callbacks?.onError?.(e as Error),
+	});
+
+	return {
+		approve,
+		sell,
+	};
+};
