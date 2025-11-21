726fad953f1bf7e0faf72297a1bd3dcff67578c3
Mithat Akbulut <marmarisco860@gmail.com>
2025-11-21 16:32:46 +0300

    fix inline snapshot

--- sdk/src/react/hooks/transactions/useGenerateSellTransaction.test.tsx
+++ sdk/src/react/hooks/transactions/useGenerateSellTransaction.test.tsx
@@ -61,7 +61,8 @@ describe('useGenerateSellTransaction', () => {
 			      "to": "0x1234567890123456789012345678901234567890",
 			      "value": "0",
 			    },
-			  ],			]
+			  ],
+			]
 		`);
 	});
 
