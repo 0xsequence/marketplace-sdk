634aab23ae6bb965dfdf37e86d864a5bfdf61dc3
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-12-10 14:06:04 +0200

    only load wallet connect clientside

--- sdk/src/react/ssr/index.ts
+++ sdk/src/react/ssr/index.ts
@@ -1,1 +1,7 @@
 export * from './create-ssr-client';
+
+// Re-export server-safe utilities for SSR usage
+export * from '../_internal/api/builder-api';
+export * from '../../consts';
+export * from '../../types';
+export * from '../../utils';
