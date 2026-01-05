be409269de9fdb8fc8892f193e6fef24c192ba50
Alexander Kolberg <alexander.kolberg@gmail.com>
2026-01-05 12:46:46 +0100

    remove design-system-v2 alias, pin deps to match 0xtrails

--- sdk/src/react/providers/modal-provider.tsx
+++ sdk/src/react/providers/modal-provider.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { SequenceCheckoutProvider } from '@0xsequence/checkout';
-import { ThemeProvider } from '@0xsequence/design-system-v2';
 import type { ReactNode } from 'react';
-//import { marketplaceApiURL } from '../_internal';
 import { useConfig } from '../hooks';
 import SwitchChainErrorModal from '../ui/modals/_internal/components/switchChainErrorModal';
 import TransactionStatusModal from '../ui/modals/_internal/components/transactionStatusModal';
@@ -22,30 +20,25 @@ export const ModalProvider = ({ children }: ModalProviderProps) => {
 	const sdkConfig = useConfig();
 	const { shadowDom, experimentalShadowDomCssOverride } = sdkConfig;
 
-	//const overrides = sdkConfig._internal?.overrides?.api?.marketplace;
-	//const _marketplaceApiUrl = overrides?.url || marketplaceApiURL(overrides?.env || 'production');
-
 	return (
 		<>
 			{children}
 
-			<ThemeProvider>
-				<SequenceCheckoutProvider>
-					<ShadowRoot
-						enabled={shadowDom ?? true}
-						customCSS={experimentalShadowDomCssOverride}
-					>
-						<CreateListingModal />
-						<MakeOfferModal />
-						<TransferModal />
-						<SellModal />
-						<BuyModal />
-						{/* Helper modals */}
-						<SwitchChainErrorModal />
-						<TransactionStatusModal />
-					</ShadowRoot>
-				</SequenceCheckoutProvider>
-			</ThemeProvider>
+			<SequenceCheckoutProvider>
+				<ShadowRoot
+					enabled={shadowDom ?? true}
+					customCSS={experimentalShadowDomCssOverride}
+				>
+					<CreateListingModal />
+					<MakeOfferModal />
+					<TransferModal />
+					<SellModal />
+					<BuyModal />
+					{/* Helper modals */}
+					<SwitchChainErrorModal />
+					<TransactionStatusModal />
+				</ShadowRoot>
+			</SequenceCheckoutProvider>
 		</>
 	);
 };
