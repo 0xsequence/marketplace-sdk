1f725da4126f771e09758d002b8cbdc3d2b608fd
Mithat Akbulut <marmarisco860@gmail.com>
2026-01-19 14:43:34 +0300

    fix(ErrorLogBox): add wrap-anywhere class to improve text wrapping

--- sdk/src/react/ui/components/_internals/ErrorLogBox.tsx
+++ sdk/src/react/ui/components/_internals/ErrorLogBox.tsx
@@ -29,7 +29,7 @@ export const ErrorLogBox = ({
 	};
 
 	return (
-		<div className="relative max-h-96 overflow-y-auto rounded-lg border border-red-900 bg-[#2b0000] p-3">
+		<div className="wrap-anywhere relative max-h-96 overflow-y-auto rounded-lg border border-red-900 bg-[#2b0000] p-3">
 			<div className="flex items-start gap-3">
 				<WarningIcon
 					className="absolute mt-0.5 flex-shrink-0 text-red-500"
