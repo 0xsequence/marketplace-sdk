c5344ec59f7d2fae3512ff9ba94099b8f6dc0301
Alexander Kolberg <alexander.kolberg@gmail.com>
2026-03-06 14:06:57 +0200

    normalize sdk marketplace helpers

--- sdk/src/utils/getConduitAddressForOrderbook.ts
+++ sdk/src/utils/getConduitAddressForOrderbook.ts
@@ -4,6 +4,7 @@ import {
 	SEQUENCE_MARKET_V2_ADDRESS,
 } from '../consts';
 import { OrderbookKind } from '../types';
+import { normalizeOrderbookKind } from './normalizeMarketplace';
 
 /**
  * OpenSea Conduit address
@@ -12,25 +13,16 @@ import { OrderbookKind } from '../types';
 export const OPENSEA_SEAPORT_CONDUIT_ADDRESS =
 	'0x1E0049783F008A0085193E00003D00cd54003c71' as Address;
 
-/**
- * Magic Eden Conduit address
- * https://etherscan.io/address/0x2052f8A2Ff46283B30084e5d84c89A2fdBE7f74b
- */
-export const MAGICEDEN_CONDUIT_ADDRESS =
-	'0x2052f8A2Ff46283B30084e5d84c89A2fdBE7f74b' as Address;
-
 export function getConduitAddressForOrderbook(
 	orderbookKind: OrderbookKind | undefined,
 ): Address | undefined {
-	switch (orderbookKind) {
+	switch (normalizeOrderbookKind(orderbookKind)) {
 		case OrderbookKind.sequence_marketplace_v1:
 			return SEQUENCE_MARKET_V1_ADDRESS as Address;
 		case OrderbookKind.sequence_marketplace_v2:
 			return SEQUENCE_MARKET_V2_ADDRESS as Address;
 		case OrderbookKind.opensea:
 			return OPENSEA_SEAPORT_CONDUIT_ADDRESS;
-		case OrderbookKind.magic_eden:
-			return MAGICEDEN_CONDUIT_ADDRESS;
 		default:
 			// Default to v2 for unknown orderbooks
 			return SEQUENCE_MARKET_V2_ADDRESS as Address;
